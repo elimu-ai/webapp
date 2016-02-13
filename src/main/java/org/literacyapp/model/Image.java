2a5a109d7b0588b9bd32e418f535b798b85da0c3
Jo Grimstad <jogrimst@gmail.com>
2016-02-13 12:42:03 +0100

    #QfNa842Q Added ImageType to Image

--- src/main/java/org/literacyapp/model/Image.java
+++ src/main/java/org/literacyapp/model/Image.java
@@ -2,12 +2,16 @@ package org.literacyapp.model;
 
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
+import org.literacyapp.model.enums.ImageType;
 
 @Entity
 public class Image extends BaseEntity {
 
     @NotNull
     private String title;
+    
+    @NotNull
+    private ImageType imageType;
 
     public String getTitle() {
         return title;
@@ -15,5 +19,13 @@ public class Image extends BaseEntity {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public ImageType getImageType() {
+        return imageType;
+    }
+
+    public void setImageType(ImageType imageType) {
+        this.imageType = imageType;
     }
 }
