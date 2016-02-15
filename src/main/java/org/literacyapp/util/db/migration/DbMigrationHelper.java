ae1094c02bcfe0fb88bdb1d8ae4aa1a9b16e2d75
Jo Grimstad <jogrimst@gmail.com>
2016-02-15 16:34:46 +0100

    #wQzScteD Fixed NullPointerException

--- src/main/java/org/literacyapp/util/db/migration/DbMigrationHelper.java
+++ src/main/java/org/literacyapp/util/db/migration/DbMigrationHelper.java
@@ -7,6 +7,7 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.literacyapp.dao.DbMigrationDao;
 import org.literacyapp.model.DbMigration;
@@ -56,10 +57,11 @@ public class DbMigrationHelper {
                 // Look up SQL scripts from src/main/resources/db/migration
                 for (int scriptVersion = (versionOfMostRecentMigration + 1); scriptVersion <= pomVersionAsInteger; scriptVersion++) {
                     URL url = getClass().getClassLoader().getResource("db/migration/" + scriptVersion + ".sql");
-                    File sqlFile = new File(url.getFile());
-                    logger.info("Looking up file \"" + sqlFile.getAbsolutePath() + "\"...");
-                    if (sqlFile.exists()) {
+                    logger.info("Looking up file \"" + url.getPath() + "\"...");
+                    if (StringUtils.isNotBlank(url.getFile())) {
                         logger.info("Migration script found for version " + scriptVersion);
+                        
+                        File sqlFile = new File(url.getFile());
                         
                         String script = "";
                         
