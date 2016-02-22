416ffeca705449ae7a10673872fdccda8e9e26ad
Jo Grimstad <jogrimst@gmail.com>
2016-02-22 15:39:13 +0100

    #wQzScteD Improved DB migration

--- src/test/java/org/literacyapp/util/VersionHelperTest.java
+++ src/test/java/org/literacyapp/util/VersionHelperTest.java
@@ -10,21 +10,24 @@ public class VersionHelperTest {
     @Test
     public void testCreate() {
         String pomVersion = "1.0.0";
-        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(100000000));
+        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(1000000));
         
         pomVersion = "1.0.0-SNAPSHOT";
-        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(100000000));
+        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(1000000));
         
         pomVersion = "1.0.1";
-        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(100000100));
+        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(1000001));
         
         pomVersion = "1.1.0";
-        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(100100000));
+        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(1001000));
         
         pomVersion = "1.1.1";
-        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(100100100));
+        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(1001001));
         
         pomVersion = "1.0.11";
-        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(100000110));
+        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(1000011));
+        
+        pomVersion = "11.0.11";
+        assertThat(VersionHelper.getPomVersionAsInteger(pomVersion), is(11000011));
     }
 }
