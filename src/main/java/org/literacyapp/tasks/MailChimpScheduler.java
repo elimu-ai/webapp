b4ffc063598d64f5cf998da9b0f4ef997552d1b0
Jo Grimstad <jo@literacyapp.org>
2016-04-10 11:29:53 +0200

    #ccEm9bQ7 Subscribe new contributors to mailing list

---
+++ src/main/java/org/literacyapp/tasks/MailChimpScheduler.java
@@ -0,0 +1,67 @@
+package org.literacyapp.tasks;
+
+import java.util.List;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.literacyapp.dao.ContributorDao;
+import org.literacyapp.model.Contributor;
+import org.literacyapp.model.enums.Environment;
+import org.literacyapp.util.MailChimpApiHelper;
+import org.literacyapp.web.context.EnvironmentContextLoaderListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailChimpScheduler {
+    
+    private Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private ContributorDao contributorDao;
+    
+    @Scheduled(cron="00 10 * * * *") // 10 past every hour
+    public synchronized void executeImport() {
+        logger.info("executeImport");
+        
+        if (EnvironmentContextLoaderListener.env != Environment.PROD) {
+            return;
+        }
+        
+        List<Contributor> contributors = contributorDao.readAll();
+        logger.info("contributors.size(): " + contributors.size());
+        for (Contributor contributor : contributors) {
+            // Check if contributor is already subscribed to the mailing list
+            String memberInfo = MailChimpApiHelper.getMemberInfo(contributor.getEmail());
+            if (StringUtils.isBlank(memberInfo)) {
+                MailChimpApiHelper.subscribeMember(contributor);
+            }
+        }
+        
+        logger.info("executeImport complete");
+    }
+    
+    /**
+     * Sync data registered for existing subscribers.
+     */
+    @Scheduled(cron="00 20 * * * *") // 20 past every hour
+    public synchronized void executeDataSync() {
+        logger.info("executeDataSync");
+        
+        if (EnvironmentContextLoaderListener.env != Environment.PROD) {
+            return;
+        }
+        
+        List<Contributor> contributors = contributorDao.readAll();
+        logger.info("contributors.size(): " + contributors.size());
+        for (Contributor contributor : contributors) {
+            String memberInfo = MailChimpApiHelper.getMemberInfo(contributor.getEmail());
+            if (StringUtils.isNotBlank(memberInfo)) {
+                // Sync Contributor data with mailing list
+                MailChimpApiHelper.updateTeams(contributor.getEmail(), contributor.getTeams());
+            }
+        }
+        
+        logger.info("executeDataSync complete");
+    }
+}
