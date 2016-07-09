3b69acd17355e9c3275703d48df536c0136da312
Jo Grimstad <jo@literacyapp.org>
2016-07-09 23:15:55 +0300

    #qfPafYk0 Share Contributor account updates in Slack channel

--- src/main/java/org/literacyapp/web/content/MainContentController.java
+++ src/main/java/org/literacyapp/web/content/MainContentController.java
@@ -13,13 +13,11 @@ import org.literacyapp.dao.NumberDao;
 import org.literacyapp.model.Contributor;
 import org.literacyapp.model.Image;
 import org.literacyapp.model.Number;
-import org.literacyapp.model.enums.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/content")
