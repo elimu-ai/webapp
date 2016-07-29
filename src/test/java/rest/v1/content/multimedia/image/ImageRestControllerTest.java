f596be34fc3fbd0a71d0573d88bcbeb42c173af9
Jo Grimstad <jo@literacyapp.org>
2016-07-29 12:49:49 +0300

    Added regression tests for multimedia REST controllers

---
+++ src/test/java/rest/v1/content/multimedia/image/ImageRestControllerTest.java
@@ -0,0 +1,35 @@
+package rest.v1.content.multimedia.image;
+
+import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.literacyapp.model.enums.Locale;
+import org.literacyapp.util.JsonLoader;
+import selenium.DomainHelper;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class ImageRestControllerTest {
+    
+    private Logger logger = Logger.getLogger(getClass());
+
+    @Test(expected = JSONException.class)
+    public void testList_missingParameters() {
+    	String jsonResponse = JsonLoader.loadJson(DomainHelper.getRestUrlV1() + "/content/multimedia/image/list");
+        logger.info("jsonResponse: " + jsonResponse);
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+    }
+    
+    @Test
+    public void testList_success() {
+        String jsonResponse = JsonLoader.loadJson(DomainHelper.getRestUrlV1() + "/content/multimedia/image/list" +
+                "?deviceId=abc123" + 
+                "&locale=" + Locale.EN);
+        logger.info("jsonResponse: " + jsonResponse);
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+        assertThat(jsonObject.has("result"), is(true));
+        assertThat(jsonObject.get("result"), is("success"));
+        assertThat(jsonObject.has("images"), is(true));
+    }
+}
