f071c62be90399c71d36cc8aecc48fbe7668a69b
Jo Grimstad <jo@literacyapp.org>
2016-09-01 22:47:03 +0300

    literacyapp-model-1.1.12

--- src/main/resources/db/migration/1001030.sql
+++ src/main/resources/db/migration/1001030.sql
@@ -1,3 +1,9 @@
 # 1.1.30
 
 UPDATE Content SET contentStatus = 'ACTIVE';
+
+UPDATE Application_literacySkills SET literacySkills = 'PHONEMIC_AWARENESS' WHERE literacySkills = 'SOUND_IDENTIFICATION';
+UPDATE Application_literacySkills SET literacySkills = 'LETTER_IDENTIFICATION' WHERE literacySkills = 'GRAPHEME_KNOWLEDGE';
+UPDATE Application_literacySkills SET literacySkills = 'NONWORD_READING' WHERE literacySkills = 'INVENTED_WORD_DECODING';
+UPDATE Application_literacySkills SET literacySkills = 'ORAL_READING_FLUENCY' WHERE literacySkills = 'ORAL_PASSAGE_READING';
+UPDATE Application_literacySkills SET literacySkills = 'DICTATION' WHERE literacySkills = 'SENTENCE_WRITING';
