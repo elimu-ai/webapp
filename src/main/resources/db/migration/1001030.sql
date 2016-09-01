6557644234e7d95fe8f60f3fe9b50b9c91e03790
Jo Grimstad <jo@literacyapp.org>
2016-09-01 22:52:32 +0300

    literacyapp-model-1.1.12

--- src/main/resources/db/migration/1001030.sql
+++ src/main/resources/db/migration/1001030.sql
@@ -7,3 +7,9 @@ UPDATE Application_literacySkills SET literacySkills = 'LETTER_IDENTIFICATION' W
 UPDATE Application_literacySkills SET literacySkills = 'NONWORD_READING' WHERE literacySkills = 'INVENTED_WORD_DECODING';
 UPDATE Application_literacySkills SET literacySkills = 'ORAL_READING_FLUENCY' WHERE literacySkills = 'ORAL_PASSAGE_READING';
 UPDATE Application_literacySkills SET literacySkills = 'DICTATION' WHERE literacySkills = 'SENTENCE_WRITING';
+
+UPDATE Multimedia_literacySkills SET literacySkills = 'PHONEMIC_AWARENESS' WHERE literacySkills = 'SOUND_IDENTIFICATION';
+UPDATE Multimedia_literacySkills SET literacySkills = 'LETTER_IDENTIFICATION' WHERE literacySkills = 'GRAPHEME_KNOWLEDGE';
+UPDATE Multimedia_literacySkills SET literacySkills = 'NONWORD_READING' WHERE literacySkills = 'INVENTED_WORD_DECODING';
+UPDATE Multimedia_literacySkills SET literacySkills = 'ORAL_READING_FLUENCY' WHERE literacySkills = 'ORAL_PASSAGE_READING';
+UPDATE Multimedia_literacySkills SET literacySkills = 'DICTATION' WHERE literacySkills = 'SENTENCE_WRITING';
