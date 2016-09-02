a4a10bd81e3946420cdad384bef082d8f70275a5
Jo Grimstad <jo@literacyapp.org>
2016-09-02 14:42:29 +0300

    #E5ZskBCT Update Student entity to reflect LiteracySkill enum refactoring

---
+++ src/main/resources/db/migration/1001031.sql
@@ -0,0 +1,23 @@
+# 1.1.31
+
+ALTER TABLE Student DROP COLUMN levelVocabulary;
+ALTER TABLE Student DROP COLUMN levelSoundIdentification;
+ALTER TABLE Student DROP COLUMN levelGraphemeKnowledge;
+ALTER TABLE Student DROP COLUMN levelFamiliarWordReading;
+ALTER TABLE Student DROP COLUMN levelInventedWordDecoding;
+ALTER TABLE Student DROP COLUMN levelOralPassageReading;
+ALTER TABLE Student DROP COLUMN levelReadingComprehension;
+ALTER TABLE Student DROP COLUMN levelListeningComprehension;
+ALTER TABLE Student DROP COLUMN levelSentenceWriting;
+
+ALTER TABLE Student DROP COLUMN levelOralCounting;
+ALTER TABLE Student DROP COLUMN levelOneToOneCorrespondence;
+ALTER TABLE Student DROP COLUMN levelNumberIdentification;
+ALTER TABLE Student DROP COLUMN levelQuantityDiscrimination;
+ALTER TABLE Student DROP COLUMN levelMissingNumber;
+ALTER TABLE Student DROP COLUMN levelAddition;
+ALTER TABLE Student DROP COLUMN levelSubtraction;
+ALTER TABLE Student DROP COLUMN levelMultiplication;
+ALTER TABLE Student DROP COLUMN levelWordProblems;
+ALTER TABLE Student DROP COLUMN levelShapeIdentification;
+ALTER TABLE Student DROP COLUMN levelShapeNaming;
