b1af31681ed7e58768d0ac46b1f65cb3d9c22dae
Jo Grimstad <jo@literacyapp.org>
2016-10-07 19:52:12 +0300

    #18 Restructure event inheritance

--- src/main/java/org/literacyapp/model/analytics/DeviceEvent.java
+++ src/main/java/org/literacyapp/model/analytics/DeviceEvent.java
@@ -1,16 +1,16 @@
 package org.literacyapp.model.analytics;
 
 import java.util.Calendar;
-import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import org.literacyapp.model.BaseEntity;
 import org.literacyapp.model.Device;
 
-@Entity
-public class DeviceEvent extends BaseEntity {
+@MappedSuperclass
+public abstract class DeviceEvent extends BaseEntity {
     
     @NotNull
     @ManyToOne
