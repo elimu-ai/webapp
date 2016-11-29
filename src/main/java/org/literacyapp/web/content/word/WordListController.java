0927e76e79d39737d110b6c66628ebf7040b4d53
Jo Grimstad <jo@literacyapp.org>
2016-11-29 14:06:57 +0300

    #271 Auto-generate Number Words

--- src/main/java/org/literacyapp/web/content/word/WordListController.java
+++ src/main/java/org/literacyapp/web/content/word/WordListController.java
@@ -89,149 +89,149 @@ public class WordListController {
         }
         words.add(wordOne);
 
-//        Word wordTwo = new Word();
-//        wordTwo.setLocale(locale);
-//        wordTwo.setTimeLastUpdate(Calendar.getInstance());
-//        if (locale == Locale.AR) {
-//            // TODO: set text
-//            // TODO: set phonetics
-//        } else if (locale == Locale.EN) {
-//            wordTwo.setText("two");
-//            wordTwo.setPhonetics("TODO");
-//        } else if (locale == Locale.ES) {
-//            wordTwo.setText("dos");
-//            wordTwo.setPhonetics("TODO");
-//        } else if (locale == Locale.SW) {
-//            wordTwo.setText("mbili");
-//            wordTwo.setPhonetics("TODO");
-//        }
-//        words.add(wordTwo);
-//
-//        Word wordThree = new Word();
-//        wordThree.setLocale(locale);
-//        wordThree.setTimeLastUpdate(Calendar.getInstance());
-//        if (locale == Locale.AR) {
-//            // TODO: set text
-//            // TODO: set phonetics
-//        } else if (locale == Locale.EN) {
-//            wordThree.setText("three");
-//            wordThree.setPhonetics("TODO");
-//        } else if (locale == Locale.ES) {
-//            wordThree.setText("tres");
-//            wordThree.setPhonetics("TODO");
-//        } else if (locale == Locale.SW) {
-//            wordThree.setText("tatu");
-//            wordThree.setPhonetics("TODO");
-//        }
-//        words.add(wordThree);
-//
-//        Word wordFour = new Word();
-//        wordFour.setLocale(locale);
-//        wordFour.setTimeLastUpdate(Calendar.getInstance());
-//        if (locale == Locale.AR) {
-//            // TODO: set text
-//            // TODO: set phonetics
-//        } else if (locale == Locale.EN) {
-//            wordFour.setText("four");
-//            wordFour.setPhonetics("TODO");
-//        } else if (locale == Locale.ES) {
-//            wordFour.setText("cuatro");
-//            wordFour.setPhonetics("TODO");
-//        } else if (locale == Locale.SW) {
-//            wordFour.setText("nne");
-//            wordFour.setPhonetics("TODO");
-//        }
-//        words.add(wordFour);
-//
-//        Word wordFive = new Word();
-//        wordFive.setLocale(locale);
-//        wordFive.setTimeLastUpdate(Calendar.getInstance());
-//        if (locale == Locale.AR) {
-//            // TODO: set text
-//            // TODO: set phonetics
-//        } else if (locale == Locale.EN) {
-//            wordFive.setText("five");
-//            wordFive.setPhonetics("TODO");
-//        } else if (locale == Locale.ES) {
-//            wordFive.setText("cinco");
-//            wordFive.setPhonetics("TODO");
-//        } else if (locale == Locale.SW) {
-//            wordFive.setText("tano");
-//            wordFive.setPhonetics("TODO");
-//        }
-//        words.add(wordFive);
-//
-//        Word wordSix = new Word();
-//        wordSix.setLocale(locale);
-//        wordSix.setTimeLastUpdate(Calendar.getInstance());
-//        if (locale == Locale.AR) {
-//            // TODO: set text
-//            // TODO: set phonetics
-//        } else if (locale == Locale.EN) {
-//            wordSix.setText("six");
-//            wordSix.setPhonetics("TODO");
-//        } else if (locale == Locale.ES) {
-//            wordSix.setText("seis");
-//            wordSix.setPhonetics("TODO");
-//        } else if (locale == Locale.SW) {
-//            wordSix.setText("sita");
-//            wordSix.setPhonetics("TODO");
-//        }
-//        words.add(wordSix);
-//
-//        Word wordSeven = new Word();
-//        wordSeven.setLocale(locale);
-//        wordSeven.setTimeLastUpdate(Calendar.getInstance());
-//        if (locale == Locale.AR) {
-//            // TODO: set text
-//            // TODO: set phonetics
-//        } else if (locale == Locale.EN) {
-//            wordSeven.setText("seven");
-//            wordSeven.setPhonetics("TODO");
-//        } else if (locale == Locale.ES) {
-//            wordSeven.setText("siete");
-//            wordSeven.setPhonetics("TODO");
-//        } else if (locale == Locale.SW) {
-//            wordSeven.setText("saba");
-//            wordSeven.setPhonetics("TODO");
-//        }
-//        words.add(wordSeven);
-//
-//        Word wordEight = new Word();
-//        wordEight.setLocale(locale);
-//        wordEight.setTimeLastUpdate(Calendar.getInstance());
-//        if (locale == Locale.AR) {
-//            // TODO: set text
-//            // TODO: set phonetics
-//        } else if (locale == Locale.EN) {
-//            wordEight.setText("eight");
-//            wordEight.setPhonetics("TODO");
-//        } else if (locale == Locale.ES) {
-//            wordEight.setText("ocho");
-//            wordEight.setPhonetics("TODO");
-//        } else if (locale == Locale.SW) {
-//            wordEight.setText("nane");
-//            wordEight.setPhonetics("TODO");
-//        }
-//        words.add(wordEight);
-//
-//        Word wordNine = new Word();
-//        wordNine.setLocale(locale);
-//        wordNine.setTimeLastUpdate(Calendar.getInstance());
-//        if (locale == Locale.AR) {
-//            // TODO: set text
-//            // TODO: set phonetics
-//        } else if (locale == Locale.EN) {
-//            wordNine.setText("nine");
-//            wordNine.setPhonetics("TODO");
-//        } else if (locale == Locale.ES) {
-//            wordNine.setText("nueve");
-//            wordNine.setPhonetics("TODO");
-//        } else if (locale == Locale.SW) {
-//            wordNine.setText("tisa");
-//            wordNine.setPhonetics("TODO");
-//        }
-//        words.add(wordNine);
+        Word wordTwo = new Word();
+        wordTwo.setLocale(locale);
+        wordTwo.setTimeLastUpdate(Calendar.getInstance());
+        if (locale == Locale.AR) {
+            // TODO: set text
+            // TODO: set phonetics
+        } else if (locale == Locale.EN) {
+            wordTwo.setText("two");
+            wordTwo.setPhonetics("tu");
+        } else if (locale == Locale.ES) {
+            wordTwo.setText("dos");
+//            wordTwo.setPhonetics(TODO);
+        } else if (locale == Locale.SW) {
+            wordTwo.setText("mbili");
+//            wordTwo.setPhonetics(TODO);
+        }
+        words.add(wordTwo);
+
+        Word wordThree = new Word();
+        wordThree.setLocale(locale);
+        wordThree.setTimeLastUpdate(Calendar.getInstance());
+        if (locale == Locale.AR) {
+            // TODO: set text
+            // TODO: set phonetics
+        } else if (locale == Locale.EN) {
+            wordThree.setText("three");
+            wordThree.setPhonetics("θri");
+        } else if (locale == Locale.ES) {
+            wordThree.setText("tres");
+//            wordThree.setPhonetics(TODO);
+        } else if (locale == Locale.SW) {
+            wordThree.setText("tatu");
+//            wordThree.setPhonetics(TODO);
+        }
+        words.add(wordThree);
+
+        Word wordFour = new Word();
+        wordFour.setLocale(locale);
+        wordFour.setTimeLastUpdate(Calendar.getInstance());
+        if (locale == Locale.AR) {
+            // TODO: set text
+            // TODO: set phonetics
+        } else if (locale == Locale.EN) {
+            wordFour.setText("four");
+            wordFour.setPhonetics("fɔr");
+        } else if (locale == Locale.ES) {
+            wordFour.setText("cuatro");
+//            wordFour.setPhonetics(TODO);
+        } else if (locale == Locale.SW) {
+            wordFour.setText("nne");
+//            wordFour.setPhonetics(TODO);
+        }
+        words.add(wordFour);
+
+        Word wordFive = new Word();
+        wordFive.setLocale(locale);
+        wordFive.setTimeLastUpdate(Calendar.getInstance());
+        if (locale == Locale.AR) {
+            // TODO: set text
+            // TODO: set phonetics
+        } else if (locale == Locale.EN) {
+            wordFive.setText("five");
+            wordFive.setPhonetics("faɪv");
+        } else if (locale == Locale.ES) {
+            wordFive.setText("cinco");
+//            wordFive.setPhonetics(TODO);
+        } else if (locale == Locale.SW) {
+            wordFive.setText("tano");
+//            wordFive.setPhonetics(TODO);
+        }
+        words.add(wordFive);
+
+        Word wordSix = new Word();
+        wordSix.setLocale(locale);
+        wordSix.setTimeLastUpdate(Calendar.getInstance());
+        if (locale == Locale.AR) {
+            // TODO: set text
+            // TODO: set phonetics
+        } else if (locale == Locale.EN) {
+            wordSix.setText("six");
+            wordSix.setPhonetics("sɪks");
+        } else if (locale == Locale.ES) {
+            wordSix.setText("seis");
+//            wordSix.setPhonetics(TODO);
+        } else if (locale == Locale.SW) {
+            wordSix.setText("sita");
+//            wordSix.setPhonetics(TODO);
+        }
+        words.add(wordSix);
+
+        Word wordSeven = new Word();
+        wordSeven.setLocale(locale);
+        wordSeven.setTimeLastUpdate(Calendar.getInstance());
+        if (locale == Locale.AR) {
+            // TODO: set text
+            // TODO: set phonetics
+        } else if (locale == Locale.EN) {
+            wordSeven.setText("seven");
+            wordSeven.setPhonetics("ˈsɛvən");
+        } else if (locale == Locale.ES) {
+            wordSeven.setText("siete");
+//            wordSeven.setPhonetics(TODO);
+        } else if (locale == Locale.SW) {
+            wordSeven.setText("saba");
+//            wordSeven.setPhonetics(TODO);
+        }
+        words.add(wordSeven);
+
+        Word wordEight = new Word();
+        wordEight.setLocale(locale);
+        wordEight.setTimeLastUpdate(Calendar.getInstance());
+        if (locale == Locale.AR) {
+            // TODO: set text
+            // TODO: set phonetics
+        } else if (locale == Locale.EN) {
+            wordEight.setText("eight");
+            wordEight.setPhonetics("eɪt");
+        } else if (locale == Locale.ES) {
+            wordEight.setText("ocho");
+//            wordEight.setPhonetics(TODO);
+        } else if (locale == Locale.SW) {
+            wordEight.setText("nane");
+//            wordEight.setPhonetics(TODO);
+        }
+        words.add(wordEight);
+
+        Word wordNine = new Word();
+        wordNine.setLocale(locale);
+        wordNine.setTimeLastUpdate(Calendar.getInstance());
+        if (locale == Locale.AR) {
+            // TODO: set text
+            // TODO: set phonetics
+        } else if (locale == Locale.EN) {
+            wordNine.setText("nine");
+            wordNine.setPhonetics("naɪn");
+        } else if (locale == Locale.ES) {
+            wordNine.setText("nueve");
+//            wordNine.setPhonetics(TODO);
+        } else if (locale == Locale.SW) {
+            wordNine.setText("tisa");
+//            wordNine.setPhonetics(TODO);
+        }
+        words.add(wordNine);
         
         return words;
     }
