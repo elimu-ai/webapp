d7346786be4c05465b700a504859e877787c68f9
Jo Grimstad <jo@literacyapp.org>
2017-01-14 21:18:16 +0300

    #316 Fix duplicate entry exception during video labelling (letters/numbers/words)

--- src/main/resources/db/migration/1001059.sql
+++ src/main/resources/db/migration/1001059.sql
@@ -1,3 +1,10 @@
-# 1.1.56
+# 1.1.59
 
-ALTER TABLE StoryBook MODIFY gradeLevel VARCHAR(255) NOT NULL;
+ALTER TABLE Video_Letter DROP PRIMARY KEY;
+ALTER TABLE Video_Letter ADD PRIMARY KEY (Video_id, letters_id);
+
+ALTER TABLE Video_Number DROP PRIMARY KEY;
+ALTER TABLE Video_Number ADD PRIMARY KEY (Video_id, numbers_id);
+
+ALTER TABLE Video_Word DROP PRIMARY KEY;
+ALTER TABLE Video_Word ADD PRIMARY KEY (Video_id, words_id);
