05bd6688dfb8531a558d2dfbaeb00186960d8998
Jo Grimstad <jo@literacyapp.org>
2017-04-17 23:17:38 +0200

    #316 Fix MySQLIntegrityConstraintViolationException during labelling of Audios

---
+++ src/main/resources/db/migration/1001074.sql
@@ -0,0 +1,5 @@
+# 1.1.74
+
+ALTER TABLE Audio_Letter DROP INDEX UK_gwia9fafepf3j8qbl9yd196ok;
+ALTER TABLE Audio_Number DROP INDEX UK_4n5p3n98h6csy718skcax16eu;
+ALTER TABLE Audio_Word DROP INDEX UK_btwdj9gi58p5hvo56ui7adytq;
