2a07155631f6fb24409a6d805e566aa390db3944
Jo Grimstad <jo@literacyapp.org>
2017-05-06 14:37:46 +0200

    #270 Differentiate between upper-case and lower-case when adding X-SAMPA value of Allophones

--- src/main/java/org/literacyapp/model/DbMigration.java
+++ src/main/java/org/literacyapp/model/DbMigration.java
@@ -15,7 +15,7 @@ public class DbMigration extends BaseEntity {
     private Integer version; // 1001001, 1001002, 1001003, ... (1.1.1, 1.1.2, 1.1.3, ...)
     
     @NotNull
-    @Column(length = 2000)
+    @Column(length = 10000)
     private String script; // SQL script copied from file in src/main/resources/db/migration/<version>.sql
 
     @NotNull
