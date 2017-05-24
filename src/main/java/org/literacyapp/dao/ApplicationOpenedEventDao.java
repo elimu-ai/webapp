d134308daa05b4c1f26d8eccde900af8501db620
Jo Grimstad <jo@literacyapp.org>
2017-05-24 16:25:33 +0300

    #411 ApplicationOpenedEvent

---
+++ src/main/java/org/literacyapp/dao/ApplicationOpenedEventDao.java
@@ -0,0 +1,21 @@
+package org.literacyapp.dao;
+
+import java.util.List;
+import org.literacyapp.model.Device;
+import org.literacyapp.model.Student;
+import org.literacyapp.model.admin.Application;
+import org.literacyapp.model.analytics.ApplicationOpenedEvent;
+import org.literacyapp.model.enums.Locale;
+
+import org.springframework.dao.DataAccessException;
+
+public interface ApplicationOpenedEventDao extends GenericDao<ApplicationOpenedEvent> {
+	
+    List<ApplicationOpenedEvent> readAll(Locale locale) throws DataAccessException;
+    
+    List<ApplicationOpenedEvent> readAll(Device device) throws DataAccessException;
+    
+    List<ApplicationOpenedEvent> readAll(Application application) throws DataAccessException;
+    
+    List<ApplicationOpenedEvent> readAll(Student student) throws DataAccessException;
+}
