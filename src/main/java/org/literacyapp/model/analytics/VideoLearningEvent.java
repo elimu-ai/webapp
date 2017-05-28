1b468a63a406ba788a3e2309d39c66be171a59a8
Jo Grimstad <jo@literacyapp.org>
2017-05-28 10:53:08 +0200

    #416 Add VideoLearningEvents

--- src/main/java/org/literacyapp/model/analytics/VideoLearningEvent.java
+++ src/main/java/org/literacyapp/model/analytics/VideoLearningEvent.java
@@ -3,20 +3,20 @@ package org.literacyapp.model.analytics;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
-import org.literacyapp.model.content.Number;
+import org.literacyapp.model.content.multimedia.Video;
 
 @Entity
-public class NumberLearningEvent extends LearningEvent {
+public class VideoLearningEvent extends LearningEvent {
     
     @NotNull
     @ManyToOne
-    private Number number;
+    private Video video;
 
-    public Number getNumber() {
-        return number;
+    public Video getVideo() {
+        return video;
     }
 
-    public void setNumber(Number number) {
-        this.number = number;
+    public void setVideo(Video video) {
+        this.video = video;
     }
 }
