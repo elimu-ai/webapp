f7111f0cf083301d362e5765a457077f9b809717
Jo Grimstad <jo@literacyapp.org>
2017-06-10 20:29:24 +0200

    #268 Skip syllables that are actual words

--- src/main/java/org/literacyapp/web/content/syllable/SyllableListController.java
+++ src/main/java/org/literacyapp/web/content/syllable/SyllableListController.java
@@ -9,9 +9,11 @@ import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
 import org.literacyapp.dao.StoryBookDao;
 import org.literacyapp.dao.SyllableDao;
+import org.literacyapp.dao.WordDao;
 import org.literacyapp.model.Contributor;
 import org.literacyapp.model.content.StoryBook;
 import org.literacyapp.model.content.Syllable;
+import org.literacyapp.model.content.Word;
 import org.literacyapp.model.enums.Locale;
 import org.literacyapp.util.SyllableFrequencyHelper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +33,9 @@ public class SyllableListController {
     
     @Autowired
     private StoryBookDao storyBookDao;
+    
+    @Autowired
+    private WordDao wordDao;
 
     @RequestMapping(method = RequestMethod.GET)
     public String handleRequest(Model model, HttpSession session) {
@@ -86,13 +91,27 @@ public class SyllableListController {
 
         for (String key : syllableFrequencyMap.keySet()) {
             String syllableText = key;
-            logger.info("syllableText: " + syllableText);
+            
+            // Skip syllables that are actual words
+            // TODO: add logic to Word editing
+            Word word = wordDao.readByText(locale, syllableText);
+            if (word != null) {
+                continue;
+            }
+
+            // Skip syllables that are not digrams
+            // TODO: add support for trigrams
+            if (syllableText.length() != 2) {
+                continue;
+            }
+            
             Syllable existingSyllable = syllableDao.readByText(locale, syllableText);
             if (existingSyllable == null) {
                 Syllable syllable = new Syllable();
                 syllable.setLocale(locale);
                 syllable.setTimeLastUpdate(Calendar.getInstance());
                 syllable.setText(syllableText);
+                syllable.setUsageCount(syllableFrequencyMap.get(syllableText));
                 syllableDao.create(syllable);
             } else {
                 existingSyllable.setUsageCount(syllableFrequencyMap.get(syllableText));
--- src/test/java/org/literacyapp/util/SyllableFrequencyHelperTest.java
+++ src/test/java/org/literacyapp/util/SyllableFrequencyHelperTest.java
@@ -20,19 +20,11 @@ public class SyllableFrequencyHelperTest {
         paragraphs.add("\"Mom,\" called Lebo. \"Come and look.\"");
         storyBook.setParagraphs(paragraphs);
         Map<String, Integer> syllableFrequencyMap = SyllableFrequencyHelper.getSyllableFrequency(storyBook);
-        System.out.println("syllableFrequencyMap: " + syllableFrequencyMap);
-        
-//        assertThat(syllableFrequencyMap.get("o"), is(5));
-//        assertThat(syllableFrequencyMap.get("e"), is(3));
-//        assertThat(syllableFrequencyMap.get("l"), is(3));
-//        assertThat(syllableFrequencyMap.get("a"), is(2));
-//        assertThat(syllableFrequencyMap.get("d"), is(2));
-//        assertThat(syllableFrequencyMap.get("m"), is(2));
-//        assertThat(syllableFrequencyMap.get("b"), is(1));
-//        assertThat(syllableFrequencyMap.get("c"), is(1));
-//        assertThat(syllableFrequencyMap.get("n"), is(1));
-//        assertThat(syllableFrequencyMap.get("C"), is(1));
-//        assertThat(syllableFrequencyMap.get("L"), is(1));
-//        assertThat(syllableFrequencyMap.get("M"), is(1));
+        assertThat(syllableFrequencyMap.get("mom"), is(1));
+        assertThat(syllableFrequencyMap.get("called"), is(1));
+        assertThat(syllableFrequencyMap.get("lebo"), is(1));
+        assertThat(syllableFrequencyMap.get("come"), is(1));
+        assertThat(syllableFrequencyMap.get("and"), is(1));
+        assertThat(syllableFrequencyMap.get("look"), is(1));
     }
 }
