9e63a95da56088040845ebda8911184d0ae97dac
raidenovski <nediarovic@gmail.com>
2017-09-20 18:20:09 +0200

    Removing unused packages

--- src/test/java/selenium/web/WelcomePageTest.java
+++ src/test/java/selenium/web/WelcomePageTest.java
@@ -5,10 +5,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.MethodRule;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
 import selenium.DomainHelper;
 
