cf595b5245367e34eba81b7917e91cc2f0f5ffab
Jo Grimstad <jo.grimstad@elimu.ai>
2017-10-02 23:16:20 +0200

    #538 Add AppGroup entity

--- src/main/java/ai/elimu/model/project/AppCategory.java
+++ src/main/java/ai/elimu/model/project/AppCategory.java
@@ -1,7 +1,11 @@
 package ai.elimu.model.project;
 
 import ai.elimu.model.BaseEntity;
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -9,6 +13,10 @@ public class AppCategory extends BaseEntity {
     
     @NotNull
     private String name;
+    
+    @OrderColumn
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<AppGroup> appGroups;
 
     public String getName() {
         return name;
@@ -16,5 +24,13 @@ public class AppCategory extends BaseEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<AppGroup> getAppGroups() {
+        return appGroups;
+    }
+
+    public void setAppGroups(List<AppGroup> appGroups) {
+        this.appGroups = appGroups;
     }
 }
