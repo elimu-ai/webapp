80c60612d66681b0ce021b2c58c86c2b7bc4f378
Jo Grimstad <jo.grimstad@elimu.ai>
2017-11-12 19:25:26 +0100

    #584 Create integration test for valid license

--- src/test/java/rest/v1/project/license/LicenseRestControllerTest.java
+++ src/test/java/rest/v1/project/license/LicenseRestControllerTest.java
@@ -13,6 +13,19 @@ public class LicenseRestControllerTest {
     private Logger logger = Logger.getLogger(getClass());
     
     @Test
+    public void testRead() {
+        String jsonResponse = JsonLoader.loadJson(DomainHelper.getRestUrlV1() + "/project/license" +
+                "?licenseEmail=info%2Btest@elimu.ai" + 
+                "&licenseNumber=b3fb-5e67-98f8-984a");
+        logger.info("jsonResponse: " + jsonResponse);
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+        assertThat(jsonObject.has("result"), is(true));
+        assertThat(jsonObject.get("result"), is("success"));
+        assertThat(jsonObject.has("appCollectionId"), is(true));
+        assertThat(jsonObject.get("appCollectionId"), is(2));
+    }
+    
+    @Test
     public void testRead_invalid() {
         String jsonResponse = JsonLoader.loadJson(DomainHelper.getRestUrlV1() + "/project/license" +
                 "?licenseEmail=info@elimu.ai" + 
