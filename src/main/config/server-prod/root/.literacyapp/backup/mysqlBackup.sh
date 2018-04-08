1aabccf5ee226bd83c64e03beef94419609406b4
Jo Grimstad <jo.grimstad@elimu.ai>
2018-04-08 14:19:43 +0200

    Database backup
    - Moved file deletion to the bottom of the script

--- src/main/config/server-prod/root/.literacyapp/backup/mysqlBackup.sh
+++ src/main/config/server-prod/root/.literacyapp/backup/mysqlBackup.sh
@@ -11,9 +11,6 @@ fi
 
 mysqldump -c -u literacyapp-user -p ************ literacyapp > database/literacyapp_`date +%Y"-"%m"-"%d`.sql
 
-# Remove files older than 7 days
-find database -type f -mtime +6 -exec rm {} \;
-
 # Copy the backup to the test server
 DUMP_FILE=/root/.literacyapp/backup/database/literacyapp_`date +%Y"-"%m"-"%d`.sql
 echo "Copying latest DUMP file to test server... ($DUMP_FILE)"
@@ -22,3 +19,7 @@ DUMP_FILE_TEST=/root/.literacyapp/backup_prod/database/literacyapp_`date +%Y"-"%
 echo "Copying to luna344:$DUMP_FILE_TEST"
 scp $DUMP_FILE root@luna344:$DUMP_FILE_TEST
 echo "Copy complete"
+
+# Remove files older than 7 days
+find database -type f -mtime +6 -exec rm {} \;
+
