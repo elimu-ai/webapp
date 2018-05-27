b0e6eaf6297dcf5df415a64b94771911dbb7113c
Jo Grimstad <jo.grimstad@elimu.ai>
2018-05-27 18:05:14 +0200

    Remove files older than 30 days

--- src/main/config/server-test/root/.literacyapp/backup_prod/removeOldBackups.sh
+++ src/main/config/server-test/root/.literacyapp/backup_prod/removeOldBackups.sh
@@ -3,5 +3,5 @@
 # Navigate to the "backup" directory of the application
 cd /root/.literacyapp/backup_prod/
 
-# Remove files older than 14 days
-find database -type f -mtime +13 -exec rm {} \;
+# Remove files older than 30 days
+find database -type f -mtime +29 -exec rm {} \;
