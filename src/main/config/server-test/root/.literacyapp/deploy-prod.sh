181401332eff7ba6b1b8b182f777cdda0b96b733
Jo Grimstad <jo.grimstad@elimu.ai>
2018-06-10 18:43:07 +0200

    #472 Rebrand to elimu.ai
    - "literacyapp" --> "webapp"

--- src/main/config/server-test/root/.literacyapp/deploy-prod.sh
+++ src/main/config/server-test/root/.literacyapp/deploy-prod.sh
@@ -1,9 +1,9 @@
-WARFILE=/var/lib/jenkins/jobs/webapp-2-precompile-jsps/workspace/target/literacyapp-SNAPSHOT.war
-PROD_SERVER=malta2253
+WARFILE=/var/lib/jenkins/workspace/webapp-2-precompile-jsps/target/webapp-SNAPSHOT.war
+PROD_SERVER=malta2797
 echo "Copying latest WAR file to prod server '$PROD_SERVER'... ($WARFILE)"
 echo "Time stamp: $(stat -c %y $WARFILE)"
 
-WARFILE_PROD=/tmp/literacyapp-SNAPSHOT.war
+WARFILE_PROD=/tmp/webapp-SNAPSHOT.war
 scp $WARFILE root@$PROD_SERVER:$WARFILE_PROD
 echo "Copy complete"
 
@@ -12,7 +12,7 @@ ssh root@$PROD_SERVER echo "JAVA_OPTIONS: $JAVA_OPTIONS"
 ssh root@$PROD_SERVER echo "Stopping prod server..."
 ssh root@$PROD_SERVER $JETTY_HOME/bin/jetty.sh stop
 ssh root@$PROD_SERVER echo "Deploying WAR file: $WARFILE_PROD $(stat -c %y $WARFILE)"
-ssh root@$PROD_SERVER cp $WARFILE_PROD $JETTY_HOME/webapps/literacyapp.war
+ssh root@$PROD_SERVER cp $WARFILE_PROD $JETTY_HOME/webapps/webapp.war
 ssh root@$PROD_SERVER echo "Starting prod server..."
 ssh root@$PROD_SERVER $JETTY_HOME/bin/jetty.sh start
 
