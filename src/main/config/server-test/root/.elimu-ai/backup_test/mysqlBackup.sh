6827da23d81ddad2aecf58dc91e2b8e460d51c3a
Nya Elimu <nya@elimu.ai>
2018-12-25 15:53:55 +0000

    QA
    Copy the backup to the prod server

--- src/main/config/server-test/root/.elimu-ai/backup_test/mysqlBackup.sh
+++ src/main/config/server-test/root/.elimu-ai/backup_test/mysqlBackup.sh
@@ -11,5 +11,14 @@ fi
 
 mysqldump -c -u literacyapp-user -p************ literacyapp > database/webapp_`date +%Y"-"%m"-"%d`.sql
 
+# Copy the backup to the prod server
+DUMP_FILE=/root/.elimu-ai/backup_test/database/webapp_`date +%Y"-"%m"-"%d`.sql
+echo "Copying latest DUMP file to prod server... ($DUMP_FILE)"
+echo "Time stamp: $(stat -c %y $DUMP_FILE)"
+DUMP_FILE_PROD=/root/.elimu-ai/backup_test/database/webapp_`date +%Y"-"%m"-"%d`.sql
+echo "Copying to elimu.ai:$DUMP_FILE_PROD"
+scp $DUMP_FILE root@elimu.ai:$DUMP_FILE_PROD
+echo "Copy complete"
+
 # Remove files older than 15 days
 find database -type f -mtime +14 -exec rm {} \;
