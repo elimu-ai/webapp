7b89e89a71b7dd5e7677b9416963da5226eef581
Nya Elimu <nya@elimu.ai>
2019-12-31 13:07:57 +0800

    #702 Delete unused/unnecessary code
    - Delete ApplicationVersionStatus

--- src/main/java/ai/elimu/dao/ApplicationVersionDao.java
+++ src/main/java/ai/elimu/dao/ApplicationVersionDao.java
@@ -3,7 +3,6 @@ package ai.elimu.dao;
 import java.util.List;
 import ai.elimu.model.admin.Application;
 import ai.elimu.model.admin.ApplicationVersion;
-import ai.elimu.model.enums.admin.ApplicationVersionStatus;
 
 import org.springframework.dao.DataAccessException;
 
@@ -12,6 +11,4 @@ public interface ApplicationVersionDao extends GenericDao<ApplicationVersion> {
     ApplicationVersion read(Application application, Integer versionCode) throws DataAccessException;
     
     List<ApplicationVersion> readAll(Application application) throws DataAccessException;
-    
-    List<ApplicationVersion> readAllByStatus(ApplicationVersionStatus applicationVersionStatus) throws DataAccessException;
 }
