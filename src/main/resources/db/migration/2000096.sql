1db978be95432d8a408e8f22d5288dc269fe89fe
Nya Elimu <nya@elimu.ai>
2020-01-08 15:34:35 +0800

    Update 2000096.sql
    - Fixed error in migration script

--- src/main/resources/db/migration/2000096.sql
+++ src/main/resources/db/migration/2000096.sql
@@ -3,4 +3,4 @@
 DELETE FROM VideoRevisionEvent WHERE contributor_id IN (SELECT id FROM Contributor WHERE locale = 'AR');
 DELETE FROM WordRevisionEvent WHERE contributor_id IN (SELECT id FROM Contributor WHERE locale = 'AR');
 DELETE FROM SignOnEvent WHERE contributor_id IN (SELECT id FROM Contributor WHERE locale = 'AR');
-DELETE FROM FROM Contributor WHERE locale = 'AR';
+DELETE FROM Contributor WHERE locale = 'AR';
