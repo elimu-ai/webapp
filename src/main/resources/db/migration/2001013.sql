88e4e1839c7c25d1801c60698ffc1a50389ce906
Nya Elimu <nya@elimu.ai>
2020-02-06 13:25:46 +0800

    #766 Empty `Contributor` table
    - Delete all Contributors except those with ADMIN/ANALYST roles.

--- src/main/resources/db/migration/2001013.sql
+++ src/main/resources/db/migration/2001013.sql
@@ -3,3 +3,4 @@
 DROP TABLE WordRevisionEvent;
 DROP TABLE VideoRevisionEvent;
 DROP TABLE SignOnEvent;
+DELETE FROM Contributor WHERE id NOT IN (SELECT Contributor_id FROM Contributor_roles WHERE roles='ADMIN' OR roles='ANALYST');
