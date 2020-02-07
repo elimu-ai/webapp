c7cc18adbb025a0755b2d686b4cfa98261d989c8
Nya Elimu <nya@elimu.ai>
2020-02-07 16:33:54 +0800

    #772 Refactor `Language` enum
    - Added DB migration script

--- src/main/resources/db/migration/2001015.sql
+++ src/main/resources/db/migration/2001015.sql
@@ -1,3 +1,27 @@
 # 2.1.15
 
-...
+UPDATE Application SET language = 'ENG' WHERE language = 'EN';
+UPDATE Allophone SET language = 'ENG' WHERE language = 'EN';
+UPDATE Emoji SET language = 'ENG' WHERE language = 'EN';
+UPDATE Letter SET language = 'ENG' WHERE language = 'EN';
+UPDATE Number SET language = 'ENG' WHERE language = 'EN';
+UPDATE StoryBook SET language = 'ENG' WHERE language = 'EN';
+UPDATE Syllable SET language = 'ENG' WHERE language = 'EN';
+UPDATE Word SET language = 'ENG' WHERE language = 'EN';
+UPDATE Audio SET language = 'ENG' WHERE language = 'EN';
+UPDATE Image SET language = 'ENG' WHERE language = 'EN';
+UPDATE Video SET language = 'ENG' WHERE language = 'EN';
+UPDATE Contributor SET language = 'ENG' WHERE language = 'EN';
+
+UPDATE Application SET language = 'SWA' WHERE language = 'SW';
+UPDATE Allophone SET language = 'SWA' WHERE language = 'SW';
+UPDATE Emoji SET language = 'SWA' WHERE language = 'SW';
+UPDATE Letter SET language = 'SWA' WHERE language = 'SW';
+UPDATE Number SET language = 'SWA' WHERE language = 'SW';
+UPDATE StoryBook SET language = 'SWA' WHERE language = 'SW';
+UPDATE Syllable SET language = 'SWA' WHERE language = 'SW';
+UPDATE Word SET language = 'SWA' WHERE language = 'SW';
+UPDATE Audio SET language = 'SWA' WHERE language = 'SW';
+UPDATE Image SET language = 'SWA' WHERE language = 'SW';
+UPDATE Video SET language = 'SWA' WHERE language = 'SW';
+UPDATE Contributor SET language = 'SWA' WHERE language = 'SW';
