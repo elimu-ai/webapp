59887c4e324625cc2b401a12880bee80470cf2ce
Nya Elimu <nya@elimu.ai>
2020-02-22 23:37:44 +0900

    #802 Upgrade Selenium to the latest version

--- src/test/java/selenium/web/WelcomePage.java
+++ src/test/java/selenium/web/WelcomePage.java
@@ -12,7 +12,7 @@ public class WelcomePage {
     public WelcomePage(WebDriver driver) {
         this.driver = driver;
 
-        driver.findElement(By.id("welcomePage"));
+        driver.findElement(By.id("welcomePageTEST"));
         
         ErrorHelper.verifyNoScriptOrMarkupError(driver);
     }
