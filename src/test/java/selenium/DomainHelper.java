2e2014b856e40d18148fb05f4de05d1a99dc2c61
Nya Elimu <nya@elimu.ai>
2020-03-11 11:59:40 +0800

    #820 Add REST API for downloading StoryBooks
    - Addede `StoryBooksRestControllerTest`

--- src/test/java/selenium/DomainHelper.java
+++ src/test/java/selenium/DomainHelper.java
@@ -3,11 +3,16 @@ package selenium;
 public class DomainHelper {
     
     public static String getBaseUrl() {
-     //return "http://localhost:8080/webapp";
-     return "http://test.elimu.ai";
+//        return "http://localhost:8080/webapp";
+        return "http://test.elimu.ai";
+//        return "http://elimu.ai";
     }
     
     public static String getRestUrlV1() {
         return getBaseUrl() + "/rest/v1";
+    }
+    
+    public static String getRestUrlV2() {
+        return getBaseUrl() + "/rest/v2";
     }
 }
