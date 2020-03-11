1e67bc9859f0ed8393d70f7344f5142aa11ddf7c
Nya Elimu <nya@elimu.ai>
2020-03-11 13:34:51 +0800

    Update StoryBooksRestControllerTest.java

--- src/test/java/ai/elimu/rest/v2/StoryBooksRestControllerTest.java
+++ src/test/java/ai/elimu/rest/v2/StoryBooksRestControllerTest.java
@@ -2,7 +2,8 @@ package ai.elimu.rest.v2;
 
 import ai.elimu.util.JsonLoader;
 import org.apache.log4j.Logger;
-import static org.hamcrest.CoreMatchers.is;
+import org.hamcrest.CoreMatchers;
+import static org.hamcrest.CoreMatchers.*;
 import org.json.JSONObject;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
@@ -17,9 +18,14 @@ public class StoryBooksRestControllerTest {
         String jsonResponse = JsonLoader.loadJson(DomainHelper.getRestUrlV2() + "/content/storybooks");
         logger.info("jsonResponse: " + jsonResponse);
         JSONObject jsonObject = new JSONObject(jsonResponse);
+        
         assertThat(jsonObject.has("result"), is(true));
         assertThat(jsonObject.get("result"), is("success"));
+        
         assertThat(jsonObject.has("storyBooks"), is(true));
         assertThat(jsonObject.getJSONArray("storyBooks").length() > 0, is(true));
+        
+        JSONObject storyBookJsonObject = jsonObject.getJSONArray("storyBooks").getJSONObject(0);
+        assertThat(storyBookJsonObject.getString("title"), not(nullValue()));
     }
 }
