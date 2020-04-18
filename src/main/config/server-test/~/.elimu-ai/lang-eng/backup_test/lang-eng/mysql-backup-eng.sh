6856c3c7cda6d824c3fc83b6acfe2032e81c76d7
Nya Elimu <nya@elimu.ai>
2020-04-18 08:26:42 +0800

    Updated DB backup scripts (TEST)

---
+++ src/main/config/server-test/~/.elimu-ai/lang-eng/backup_test/lang-eng/mysql-backup-eng.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+LANGUAGE="eng"
+echo "LANGUAGE: $LANGUAGE"
+
+# Navigate to the backup directory of the webapp
+cd ~/.elimu-ai/lang-$LANGUAGE/backup_test/
+
+# Create directory 'database' if it does not already exist
+if [ ! -d "database" ]; then
+    mkdir database
+    echo "directory '$(pwd)/database' was created"
+fi
+
+mysqldump -c -u webapp-$LANGUAGE-user -p********** webapp-$LANGUAGE > database/webapp-$LANGUAGE-`date +%Y"-"%m"-"%d`.sql
+
+# Copy the backup to the prod server
+DUMP_FILE=~/.elimu-ai/lang-$LANGUAGE/backup_test/database/webapp-$LANGUAGE-`date +%Y"-"%m"-"%d`.sql
+echo "Copying latest DUMP file to prod server... ($DUMP_FILE)"
+echo "Time stamp: $(stat -c %y $DUMP_FILE)"
+DUMP_FILE_PROD=~/.elimu-ai/lang-$LANGUAGE/backup_test/database/webapp-$LANGUAGE-`date +%Y"-"%m"-"%d`.sql
+echo "Copying to $LANGUAGE.elimu.ai:$DUMP_FILE_PROD"
+scp $DUMP_FILE root@$LANGUAGE.elimu.ai:$DUMP_FILE_PROD
+echo "Copy complete"
+
+# Remove files older than 30 days
+find database -type f -mtime +29 -exec rm {} \;
+
