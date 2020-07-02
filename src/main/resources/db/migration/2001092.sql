1f8905c549ad9420d68d6d97a5c51ace32fca7cc
Nya Elimu <nya@elimu.ai>
2020-07-02 15:31:27 +0800

    #965 Fix bug: Duplicate column name 'usageCount'
    - Reverted deletion

--- src/main/resources/db/migration/2001092.sql
+++
@@ -1,8 +0,0 @@
-# 2.1.92
-
-ALTER TABLE `Audio` DROP COLUMN `usageCount`;
-ALTER TABLE `Emoji` DROP COLUMN `usageCount`;
-ALTER TABLE `Image` DROP COLUMN `usageCount`;
-ALTER TABLE `Number` DROP COLUMN `usageCount`;
-ALTER TABLE `StoryBook` DROP COLUMN `usageCount`;
-ALTER TABLE `Video` DROP COLUMN `usageCount`;
