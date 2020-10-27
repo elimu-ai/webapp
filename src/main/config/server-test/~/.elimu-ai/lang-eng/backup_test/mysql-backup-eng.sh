5a51d5a134f84407b1a4eba34ec9e12d1e20ffd4
Nya <info@elimu.ai>
2020-10-27 17:05:42 +0100

    fix Fixed DB backup script
    - Made language lower-case

--- src/main/config/server-test/~/.elimu-ai/lang-eng/backup_test/mysql-backup-eng.sh
+++ src/main/config/server-test/~/.elimu-ai/lang-eng/backup_test/mysql-backup-eng.sh
@@ -12,7 +12,7 @@ if [ ! -d "database" ]; then
     echo "directory '$(pwd)/database' was created"
 fi
 
-mysqldump -c -u webapp-$LANGUAGE-user -p********** webapp-$LANGUAGE > database/webapp-$LANGUAGE-`date +%Y"-"%m"-"%d`.sql
+mysqldump -c -u webapp-${LANGUAGE,,}-user -p********** webapp-${LANGUAGE,,} > database/webapp-$LANGUAGE-`date +%Y"-"%m"-"%d`.sql
 
 # Copy the backup to the prod server
 DUMP_FILE=~/.elimu-ai/lang-$LANGUAGE/backup_test/database/webapp-$LANGUAGE-`date +%Y"-"%m"-"%d`.sql
@@ -20,8 +20,8 @@ echo "Copying latest DUMP file to prod server... ($DUMP_FILE)"
 echo "Time stamp: $(stat -c %y $DUMP_FILE)"
 echo "File size: $(($(stat -c%s $DUMP_FILE)/1024/1024)) MB"
 DUMP_FILE_PROD=~/.elimu-ai/lang-$LANGUAGE/backup_test/database/webapp-$LANGUAGE-`date +%Y"-"%m"-"%d`.sql
-echo "Copying to $LANGUAGE.elimu.ai:$DUMP_FILE_PROD"
-scp $DUMP_FILE root@$LANGUAGE.elimu.ai:$DUMP_FILE_PROD
+echo "Copying to ${LANGUAGE,,}.elimu.ai:$DUMP_FILE_PROD"
+scp $DUMP_FILE root@${LANGUAGE,,}.elimu.ai:$DUMP_FILE_PROD
 echo "Copy complete"
 
 # Remove files older than 30 days
