be8dee99d5b4ce37c22537d4072e4ebb7a4fa204
Nya <info@elimu.ai>
2020-11-30 16:44:29 +0100

    feat: #1142 Add EDITOR role
    - Add `EDITOR`

--- src/main/java/ai/elimu/model/enums/Role.java
+++ src/main/java/ai/elimu/model/enums/Role.java
@@ -2,7 +2,8 @@ package ai.elimu.model.enums;
 
 public enum Role {
     
-    ADMIN,
-    ANALYST,
-    CONTRIBUTOR
+    ADMIN, // Access to upload and update APKs
+    ANALYST, // Access to analyze learning events and assessment events
+    CONTRIBUTOR, // Access to add and edit educational content
+    EDITOR // Same as CONTRIBUTOR, but with acceess to delete educational content
 }
