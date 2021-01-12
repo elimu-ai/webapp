0e31ba4cd282ac254b0e8563cfb86e8aa92dc6d7
Nya <info@elimu.ai>
2021-01-12 22:07:37 +0100

    #1075 Auto-select Letter-to-Allophone mappings when creating/editing Words
    - Auto-select mappings

--- src/main/java/ai/elimu/web/content/word/WordEditController.java
+++ src/main/java/ai/elimu/web/content/word/WordEditController.java
@@ -215,29 +215,31 @@ public class WordEditController {
         while (StringUtils.isNotBlank(wordText)) {
             logger.info("wordText: \"" + wordText + "\"");
             
+            boolean isMatch = false;
             for (LetterToAllophoneMapping letterToAllophoneMapping : allLetterToAllophoneMappingsOrderedByLettersLength) {
                 String letterToAllophoneMappingLetters = letterToAllophoneMapping.getLetters().stream().map(Letter::getText).collect(Collectors.joining());
                 logger.info("letterToAllophoneMappingLetters: \"" + letterToAllophoneMappingLetters + "\"");
 
                 if (wordText.startsWith(letterToAllophoneMappingLetters)) {
+                    isMatch = true;
                     logger.info("Found match at the beginning of \"" + wordText + "\"");
                     letterToAllophoneMappings.add(letterToAllophoneMapping);
 
                     // Remove the match from the word
                     wordText = wordText.substring(letterToAllophoneMappingLetters.length());
                     
-                    // Repeat the process on the remaining part of the word
                     break;
                 }
             }
             
-            // No matching mapping was found at the beginning of the word
-            if (wordText.length() > 1) {
-                // Remove the first letter, and try again
-                wordText = wordText.substring(1);
-            } else {
-                // Remove the letter
-                wordText = "";
+            if (!isMatch) {
+                if (wordText.length() > 1) {
+                    // Remove the first letter
+                    wordText = wordText.substring(1);
+                } else {
+                    // Remove the letter
+                    wordText = "";
+                }
             }
         }
         
