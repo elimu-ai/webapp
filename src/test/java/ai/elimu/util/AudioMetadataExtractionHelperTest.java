3c1775334b94b5c64216cb9ad314ccbf714f7bf1
UmenR <umendrarajapakshe@gmail.com>
2021-05-06 09:27:37 +0530

    use jaudio tagger for audio duration extraction

--- src/test/java/ai/elimu/util/AudioMetadataExtractionHelperTest.java
+++ src/test/java/ai/elimu/util/AudioMetadataExtractionHelperTest.java
@@ -7,28 +7,26 @@ import java.io.InputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
-import org.junit.Ignore;
+import static org.junit.Assert.assertThat;;
 import org.junit.Test;
 import org.springframework.core.io.ClassRelativeResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
 public class AudioMetadataExtractionHelperTest {
-    
+
     private Logger logger = LogManager.getLogger();
-    
-    @Ignore
+
     @Test
     public void testGetDurationInMilliseconds() throws IOException {
         ResourceLoader resourceLoader = new ClassRelativeResourceLoader(AudioMetadataExtractionHelper.class);
         Resource resource = resourceLoader.getResource("fourteen.mp3");
         File audioFile = resource.getFile();
         logger.debug("audioFile: " + audioFile);
-        
+
         InputStream inputStream = new FileInputStream(audioFile);
-        Long durationMs = AudioMetadataExtractionHelper.getDurationInMilliseconds(inputStream);
+        Long durationMs = AudioMetadataExtractionHelper.getDurationInMilliseconds(audioFile);
         assertThat(durationMs, not(nullValue()));
-        assertThat(durationMs, is(961)); // 00:00.961
+        assertThat(durationMs, is(1000L)); // 00:00.961
     }
 }
