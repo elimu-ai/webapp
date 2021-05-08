7def5e8d5e2d1f6b3cab5b876722e6c5ce14afe2
UmenR <umendrarajapakshe@gmail.com>
2021-05-08 12:00:05 +0530

    add unit tests for ogg and wav duration extraction

--- src/main/java/ai/elimu/util/AudioMetadataExtractionHelper.java
+++ src/main/java/ai/elimu/util/AudioMetadataExtractionHelper.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.AudioHeader;
@@ -24,21 +22,20 @@ public class AudioMetadataExtractionHelper {
     /**
      * Extracts the duration of an audio file.
      */
-    public static Long getDurationInMilliseconds(File target) {
+    public static Long getDurationInMilliseconds(File file) {
         logger.info("getDurationInMilliseconds");
 
         Long duration = null;
         try {
-            AudioFile audioFile = AudioFileIO.read(target);
+            AudioFile audioFile = AudioFileIO.read(file);
             AudioHeader audioHeader = audioFile.getAudioHeader();
-            // Convert Duration to MilliSeconds.
-            duration = (long)audioHeader.getTrackLength() * 1000;
+            // Convert duration to milliseconds.
+            duration = (long) audioHeader.getTrackLength() * 1000;
         } catch (IOException | CannotReadException | TagException |
                 ReadOnlyFileException | InvalidAudioFrameException ex) {
-            ex.printStackTrace();
             logger.error(ex);
         }
 
-        return  duration;
+        return duration;
     }
 }
--- src/test/java/ai/elimu/util/AudioMetadataExtractionHelperTest.java
+++ src/test/java/ai/elimu/util/AudioMetadataExtractionHelperTest.java
@@ -1,13 +1,11 @@
 package ai.elimu.util;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;;
+import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.springframework.core.io.ClassRelativeResourceLoader;
 import org.springframework.core.io.Resource;
@@ -18,15 +16,38 @@ public class AudioMetadataExtractionHelperTest {
     private Logger logger = LogManager.getLogger();
 
     @Test
-    public void testGetDurationInMilliseconds() throws IOException {
+    public void testGetDurationInMillisecondsMP3() throws IOException {
         ResourceLoader resourceLoader = new ClassRelativeResourceLoader(AudioMetadataExtractionHelper.class);
         Resource resource = resourceLoader.getResource("fourteen.mp3");
         File audioFile = resource.getFile();
         logger.debug("audioFile: " + audioFile);
 
-        InputStream inputStream = new FileInputStream(audioFile);
         Long durationMs = AudioMetadataExtractionHelper.getDurationInMilliseconds(audioFile);
         assertThat(durationMs, not(nullValue()));
-        assertThat(durationMs, is(1000L)); // 00:00.961
+        assertThat(durationMs, is(1000L)); // 1s
+    }
+
+    @Test
+    public void testGetDurationInMillisecondsWAV() throws IOException {
+        ResourceLoader resourceLoader = new ClassRelativeResourceLoader(AudioMetadataExtractionHelper.class);
+        Resource resource = resourceLoader.getResource("fourteen.wav");
+        File audioFile = resource.getFile();
+        logger.debug("audioFile: " + audioFile);
+
+        Long durationMs = AudioMetadataExtractionHelper.getDurationInMilliseconds(audioFile);
+        assertThat(durationMs, not(nullValue()));
+        assertThat(durationMs, is(1000L)); // 1s
+    }
+
+    @Test
+    public void testGetDurationInMillisecondsOGG() throws IOException {
+        ResourceLoader resourceLoader = new ClassRelativeResourceLoader(AudioMetadataExtractionHelper.class);
+        Resource resource = resourceLoader.getResource("example.ogg");
+        File audioFile = resource.getFile();
+        logger.debug("audioFile: " + audioFile);
+
+        Long durationMs = AudioMetadataExtractionHelper.getDurationInMilliseconds(audioFile);
+        assertThat(durationMs, not(nullValue()));
+        assertThat(durationMs, is(6000L)); // 6s
     }
 }
