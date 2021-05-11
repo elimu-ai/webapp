4f84474a7a795d281e34c3a9294a07a46bb3069c
Nya <info@elimu.ai>
2021-05-11 14:37:55 +0200

    feat: #1262 Store information about platform used when crowdsourcing content
    - Fix `java.sql.SQLSyntaxErrorException: Unknown column 'WEBAPP' in 'field list'`

--- src/main/resources/db/migration/2001191.sql
+++ src/main/resources/db/migration/2001191.sql
@@ -1,4 +1,4 @@
 # 2.1.191
 
-UPDATE `AudioContributionEvent` SET `platform` = `WEBAPP` WHERE `platform` IS NOT NULL;
-UPDATE `AudioPeerReviewEvent` SET `platform` = `WEBAPP` WHERE `platform` IS NOT NULL;
+UPDATE `AudioContributionEvent` SET `platform` = 'WEBAPP' WHERE `platform` IS NOT NULL;
+UPDATE `AudioPeerReviewEvent` SET `platform` = 'WEBAPP' WHERE `platform` IS NOT NULL;
