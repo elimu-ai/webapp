464b87e79ed8004b10918a58082892faa135f6ca
Nya <info@elimu.ai>
2021-05-27 07:52:14 +0200

    feat: #1277 Auto-generate Audios using Text-to-Speech (TTS)
    - Add utility class for generating an audio file from the Google Cloud Text-to-Speech API.

---
+++ src/main/java/ai/elimu/util/audio/GoogleCloudTextToSpeechHelper.java
@@ -0,0 +1,20 @@
+package ai.elimu.util.audio;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Utility class for connecting to Google Cloud Text-to-Speech (https://cloud.google.com/text-to-speech/) 
+ * to synthesize an audio file from text.
+ * <p />
+ * 
+ * Google Cloud Console: https://console.cloud.google.com/apis/api/texttospeech.googleapis.com
+ */
+public class GoogleCloudTextToSpeechHelper {
+    
+    private static final String GOOGLE_API_REQUEST_URL = "https://cloud.google.com/text-to-speech/docs/basics";
+    
+    private static Logger logger = LogManager.getLogger();
+    
+    // TODO
+}
