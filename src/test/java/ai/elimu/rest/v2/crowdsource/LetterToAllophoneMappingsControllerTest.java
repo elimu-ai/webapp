75e69d9497cb40e9e8f960279a83fb513bd85362
UmenR <umendrarajapakshe@gmail.com>
2021-06-09 22:24:21 +0530

    initial configuration for new unit tests

--- src/test/java/ai/elimu/rest/v2/crowdsource/LetterToAllophoneMappingsControllerTest.java
+++ src/test/java/ai/elimu/rest/v2/crowdsource/LetterToAllophoneMappingsControllerTest.java
@@ -1,11 +1,27 @@
 package ai.elimu.rest.v2.crowdsource;
 
+import ai.elimu.dao.LetterToAllophoneMappingDao;
+import ai.elimu.model.content.LetterToAllophoneMapping;
 import ai.elimu.util.JsonLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
+
+import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import selenium.DomainHelper;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -13,10 +29,28 @@ import static org.junit.Assert.assertThat;
 
 public class LetterToAllophoneMappingsControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     private Logger logger = LogManager.getLogger();
 
+    LetterToAllophoneMapping exampleLetterToAllophoneMapping = new LetterToAllophoneMapping();
+
+    @Before
+    public void setup(){
+        mockMvc = MockMvcBuilders.standaloneSetup(new LetterToAllophoneMappingsController()).build();
+    }
+
     @Test
-    public void testGetRequest() {
+    public void testGetRequest() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+                "/crowdsource/letter-to-allophone-mappings").accept(
+                MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        logger.info("sdsdsdsdsd");
+        logger.info(result.getResponse().getContentAsString());
+
         String jsonResponse = JsonLoader.loadJson(DomainHelper.getRestUrlV2() +
                 "/crowdsource/letter-to-allophone-mappings");
 
@@ -24,6 +58,7 @@ public class LetterToAllophoneMappingsControllerTest {
 
         JSONArray letterToAllophoneMappingsJSONArray = new JSONArray(jsonResponse);
         logger.info("letterToAllophoneMappingsJSONArray.length(): " + letterToAllophoneMappingsJSONArray.length());
+        logger.info(letterToAllophoneMappingsJSONArray.get(0));
         assertThat(letterToAllophoneMappingsJSONArray.length() > 0, is(true));
 
         JSONObject letterToAllophoneMappingJsonObject = letterToAllophoneMappingsJSONArray.getJSONObject(0);
