fb0ce2761bd63497ae08a31d822347787ce96387
UmenR <umendrarajapakshe@gmail.com>
2021-06-13 23:17:05 +0530

    create sample test suite

---
+++ src/test/java/dev/ai/elimu/rest/SuitTest.java
@@ -0,0 +1,9 @@
+package dev.ai.elimu.rest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({LetterToAllophoneMappingsControllerTest.class})
+public class SuitTest {
+}
