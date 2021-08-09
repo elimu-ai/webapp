fe808c52694d68bd10a41725d366bc560af07e04
UmenR <umendrarajapakshe@gmail.com>
2021-08-09 22:36:44 +0530

    add initial logic to create word peeer review event through REST API

--- src/main/java/ai/elimu/rest/v2/crowdsource/WordPeerReviewsRestController.java
+++ src/main/java/ai/elimu/rest/v2/crowdsource/WordPeerReviewsRestController.java
@@ -3,9 +3,12 @@ package ai.elimu.rest.v2.crowdsource;
 import ai.elimu.dao.ContributorDao;
 import ai.elimu.dao.WordContributionEventDao;
 import ai.elimu.dao.WordPeerReviewEventDao;
-import ai.elimu.model.contributor.Contributor;
-import ai.elimu.model.contributor.WordContributionEvent;
-import ai.elimu.model.contributor.WordPeerReviewEvent;
+import ai.elimu.model.content.multimedia.Audio;
+import ai.elimu.model.contributor.*;
+import ai.elimu.model.enums.PeerReviewStatus;
+import ai.elimu.model.enums.Platform;
+import ai.elimu.model.v2.gson.crowdsource.AudioContributionEventGson;
+import ai.elimu.model.v2.gson.crowdsource.AudioPeerReviewEventGson;
 import ai.elimu.model.v2.gson.crowdsource.WordContributionEventGson;
 import ai.elimu.rest.v2.JpaToGsonConverter;
 import ai.elimu.web.content.word.WordPeerReviewsController;
@@ -18,6 +21,7 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -118,5 +122,96 @@ public class WordPeerReviewsRestController {
         String jsonResponse = wordContributionEventsPendingPeerReviewJsonArray.toString();
         logger.info("jsonResponse: " + jsonResponse);
         return jsonResponse;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String handlePostRequest(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    @RequestBody String requestBody) {
+        logger.info("handlePostRequest");
+
+        // Validate the Contributor.
+        JSONObject jsonObject = new JSONObject();
+
+        String providerIdGoogle = request.getHeader("providerIdGoogle");
+        logger.info("providerIdGoogle: " + providerIdGoogle);
+        if (StringUtils.isBlank(providerIdGoogle)) {
+            jsonObject.put("result", "error");
+            jsonObject.put("errorMessage", "Missing providerIdGoogle");
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+
+            String jsonResponse = jsonObject.toString();
+            logger.info("jsonResponse: " + jsonResponse);
+            return jsonResponse;
+        }
+
+        // Lookup the Contributor by ID
+        Contributor contributor = contributorDao.readByProviderIdGoogle(providerIdGoogle);
+        logger.info("contributor: " + contributor);
+        if (contributor == null) {
+            jsonObject.put("result", "error");
+            jsonObject.put("errorMessage", "The Contributor was not found.");
+            response.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+
+            String jsonResponse = jsonObject.toString();
+            logger.info("jsonResponse: " + jsonResponse);
+            return jsonResponse;
+        }
+
+        try {
+            // Convert from Gson (POJO) to JPA/Hibernate
+            logger.info("requestBody: " + requestBody);
+            // Missing WordPeerReviewEventGSON in model ?????
+
+
+//            WordContributionEventGson wordPeerReviewEvent = new Gson().fromJson(requestBody, WordContributionEventGson.class);
+//            logger.info("audioPeerReviewEventGson: " + audioPeerReviewEventGson);
+//            AudioContributionEventGson audioContributionEventGson = audioPeerReviewEventGson.getAudioContributionEvent();
+//            logger.info("audioContributionEventGson: " + audioContributionEventGson);
+//            Long audioContributionEventGsonId = audioContributionEventGson.getId();
+//            logger.info("audioContributionEventGsonId: " + audioContributionEventGsonId);
+//            AudioContributionEvent audioContributionEvent = audioContributionEventDao.read(audioContributionEventGsonId);
+//            logger.info("audioContributionEvent: " + audioContributionEvent);
+//
+//            AudioPeerReviewEvent audioPeerReviewEvent = new AudioPeerReviewEvent();
+//            audioPeerReviewEvent.setContributor(contributor);
+//            audioPeerReviewEvent.setAudioContributionEvent(audioContributionEvent);
+//            audioPeerReviewEvent.setApproved(audioPeerReviewEventGson.isApproved());
+//            audioPeerReviewEvent.setComment(audioPeerReviewEventGson.getComment());
+//            audioPeerReviewEvent.setTime(audioPeerReviewEventGson.getTime());
+//            audioPeerReviewEvent.setPlatform(Platform.CROWDSOURCE_APP);
+//
+//            // Store the peer review event in the database
+//            audioPeerReviewEventDao.create(audioPeerReviewEvent);
+//
+//            // Update the audio's peer review status
+//            int approvedCount = 0;
+//            int notApprovedCount = 0;
+//            for (AudioPeerReviewEvent peerReviewEvent : audioPeerReviewEventDao.readAll(audioContributionEvent)) {
+//                if (peerReviewEvent.isApproved()) {
+//                    approvedCount++;
+//                } else {
+//                    notApprovedCount++;
+//                }
+//            }
+//            logger.info("approvedCount: " + approvedCount);
+//            logger.info("notApprovedCount: " + notApprovedCount);
+//            Audio audio = audioContributionEvent.getAudio();
+//            if (approvedCount >= notApprovedCount) {
+//                audio.setPeerReviewStatus(PeerReviewStatus.APPROVED);
+//            } else {
+//                audio.setPeerReviewStatus(PeerReviewStatus.NOT_APPROVED);
+//            }
+//            audioDao.update(audio);
+        } catch (Exception ex) {
+            logger.error(ex);
+
+            jsonObject.put("result", "error");
+            jsonObject.put("errorMessage", ex.getMessage());
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
+
+
+        return "test";
     }
 }
