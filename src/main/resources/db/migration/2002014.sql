0a031e65a592124a3e1c1e69cfbbed55222773c1
Nya <info@elimu.ai>
2021-09-09 11:29:15 +0400

    refactor: #1424 Add parent class to contribution events
    - Add DB migration script to prevent `org.hibernate.engine.jdbc.spi.SqlExceptionHelper - Data truncated for column 'timeSpentMs'`

--- src/main/resources/db/migration/2002014.sql
+++ src/main/resources/db/migration/2002014.sql
@@ -1,6 +1,7 @@
 # 2.2.14
 
 # Change timeSpentMs from NULL to NOT NULL
+UPDATE `StoryBookContributionEvent` SET `timeSpentMs` = 0 WHERE `timeSpentMs` IS NULL;
 ALTER TABLE `StoryBookContributionEvent` MODIFY `timeSpentMs` bigint(20) NOT NULL;
 
 # Add platform to pre-existing contribution events
