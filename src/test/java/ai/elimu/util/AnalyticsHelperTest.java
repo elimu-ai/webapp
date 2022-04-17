79e0d4dfe7a114e7be7693347662d560f9803267
Nya <info@elimu.ai>
2022-04-17 17:58:38 +0800

    feat: Extract versionCode from CSV filename #1546

--- src/test/java/ai/elimu/util/AnalyticsHelperTest.java
+++ src/test/java/ai/elimu/util/AnalyticsHelperTest.java
@@ -9,10 +9,19 @@ public class AnalyticsHelperTest {
     
     @Test
     public void testExtractAndroidIdFromCsvFilename() {
-        String filename = "745f90e7aae26423_storybook-learning-events_2020-05-02.csv";
+        String filename = "745f90e7aae26423_3001012_storybook-learning-events_2020-05-02.csv";
         assertThat(AnalyticsHelper.extractAndroidIdFromCsvFilename(filename), is("745f90e7aae26423"));
         
-        filename = "7161a85a0e4751cd_word-learning-events_2020-04-23.csv";
+        filename = "7161a85a0e4751cd_3001012_word-learning-events_2020-04-23.csv";
         assertThat(AnalyticsHelper.extractAndroidIdFromCsvFilename(filename), is("7161a85a0e4751cd"));
+    }
+    
+    @Test
+    public void testExtractVersionCodeFromCsvFilename() {
+        String filename = "745f90e7aae26423_3001012_storybook-learning-events_2020-05-02.csv";
+        assertThat(AnalyticsHelper.extractVersionCodeFromCsvFilename(filename), is(3001012));
+        
+        filename = "7161a85a0e4751cd_3001012_word-learning-events_2020-04-23.csv";
+        assertThat(AnalyticsHelper.extractVersionCodeFromCsvFilename(filename), is(3001012));
     }
 }
