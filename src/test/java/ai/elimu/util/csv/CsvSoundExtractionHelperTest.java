6f14fcd8fdc1cb9673a398b984bd034ada621d7c
vrudas <vrudas4work@gmail.com>
2022-07-04 00:19:44 +0300

    #1396 Covered extraction of valueIpa by tests

--- src/test/java/ai/elimu/util/csv/CsvSoundExtractionHelperTest.java
+++ src/test/java/ai/elimu/util/csv/CsvSoundExtractionHelperTest.java
@@ -9,14 +9,29 @@ import org.junit.rules.TemporaryFolder;
 import java.io.File;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.function.Function;
 
 import static ai.elimu.util.csv.CsvSoundExtractionHelper.getSoundsFromCsvBackup;
 import static java.util.Collections.emptyList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class CsvSoundExtractionHelperTest {
 
     private static final String HEADERS_LINE = "id,value_ipa,audio_id,diacritic,sound_type,usage_count";
+
+    private static <T> void verifySoundField(
+        List<Sound> sounds,
+        T expectedValue,
+        Function<Sound, T> fieldValueSupplier
+    ) {
+        assertFalse("Expecting extracted Sounds are not empty", sounds.isEmpty());
+        assertEquals("Assertion expect only one Sound object to check", 1, sounds.size());
+
+        Sound sound = sounds.get(0);
+
+        assertEquals(expectedValue, fieldValueSupplier.apply(sound));
+    }
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
@@ -45,6 +60,36 @@ public class CsvSoundExtractionHelperTest {
         List<Sound> soundsFromCsvBackup = getSoundsFromCsvBackup(soundsCsv);
 
         assertEquals(emptyList(), soundsFromCsvBackup);
+    }
+
+    @Test
+    public void extracted_sound_without_valueIpa() throws Exception {
+        Files.write(
+            soundsCsv.toPath(),
+            List.of(
+                HEADERS_LINE,
+                "5,,{,,false,VOWEL,616"
+            )
+        );
+
+        List<Sound> soundsFromCsvBackup = getSoundsFromCsvBackup(soundsCsv);
+
+        verifySoundField(soundsFromCsvBackup, "", Sound::getValueIpa);
+    }
+
+    @Test
+    public void extracted_sound_with_valueIpa() throws Exception {
+        Files.write(
+            soundsCsv.toPath(),
+            List.of(
+                HEADERS_LINE,
+                "5,æ,{,,false,VOWEL,616"
+            )
+        );
+
+        List<Sound> soundsFromCsvBackup = getSoundsFromCsvBackup(soundsCsv);
+
+        verifySoundField(soundsFromCsvBackup, "æ", Sound::getValueIpa);
     }
 
 }
