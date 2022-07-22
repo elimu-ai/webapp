383e1876af1b9f9acc5f938ac225f5d07951cb09
Nya Ξlimu <info@elimu.ai>
2022-07-22 10:12:02 +0300

    Create WordListControllerTest.java

---
+++ src/test/java/ai/elimu/web/content/word/WordListControllerTest.java
@@ -0,0 +1,44 @@
+package ai.elimu.web.content.word;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={
+    "file:src/main/webapp/WEB-INF/spring/applicationContext.xml",
+    "file:src/main/webapp/WEB-INF/spring/applicationContext-jpa.xml"
+})
+public class WordListControllerTest {
+    
+    @Autowired
+    private WordListController wordListController;
+    
+    private MockMvc mockMvc;
+    
+    @Before
+    public void setup() {
+        assertNotNull(wordListController);
+        mockMvc = MockMvcBuilders.standaloneSetup(wordListController).build();
+        assertNotNull(mockMvc);
+    }
+    
+    @Test
+    public void testHandleRequest() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/content/word/list");
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
+        assertEquals("content/word/list", mvcResult.getModelAndView().getViewName());
+    }
+}
