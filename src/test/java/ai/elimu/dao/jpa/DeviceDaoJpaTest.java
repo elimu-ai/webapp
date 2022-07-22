472ddd69695ecb2b3ee6904d6de18a236a09c45d
Tomasz <tom-smy@wp.pl>
2022-07-22 22:38:08 +0100

    1586 Improve code coverage from 11% to 15%

---
+++ src/test/java/ai/elimu/dao/jpa/DeviceDaoJpaTest.java
@@ -0,0 +1,38 @@
+package ai.elimu.dao.jpa;
+
+import ai.elimu.dao.DeviceDao;
+import ai.elimu.model.Device;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={
+        "file:src/main/webapp/WEB-INF/spring/applicationContext.xml",
+        "file:src/main/webapp/WEB-INF/spring/applicationContext-jpa.xml"
+})
+public class DeviceDaoJpaTest extends TestCase {
+
+    @Autowired
+    DeviceDao deviceDao;
+
+    @Test
+    public void testRead() {
+        deviceDao.create(getDevice("22"));
+        deviceDao.create(getDevice("44"));
+
+        assertTrue("22".equals(deviceDao.read("22").getDeviceId()));
+        assertTrue("44".equals(deviceDao.read("44").getDeviceId()));
+        assertNull(deviceDao.read("33"));
+    }
+
+    private Device getDevice(String deviceId) {
+        Device device = new Device();
+        device.setDeviceId(deviceId);
+        return device;
+    }
+
+}
