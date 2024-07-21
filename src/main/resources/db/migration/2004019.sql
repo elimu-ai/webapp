b41df6b2ea35b279db13aab7147e8fb1e49a9629
Sneha Holehonnur Sridhar <snehamysuru2000@gmail.com>
2024-07-21 16:11:35 -0400

    1677 resolving merge conflicts

---
+++ src/main/resources/db/migration/2004019.sql
@@ -0,0 +1,14 @@
+# 2.4.19
+
+# "LetterSoundCorrespondence" → "LetterSound"
+DROP TABLE IF EXISTS `LetterSound`;
+ALTER TABLE `LetterSoundCorrespondence` RENAME TO `LetterSound`;
+
+DROP TABLE IF EXISTS `LetterSound_Letter`;
+ALTER TABLE `LetterSoundCorrespondence_Letter` RENAME TO `LetterSound_Letter`;
+
+DROP TABLE IF EXISTS `LetterSound_Sound`;
+ALTER TABLE `LetterSoundCorrespondence_Sound` RENAME TO `LetterSound_Sound`;
+
+DROP TABLE IF EXISTS `Word_LetterSound`;
+ALTER TABLE `Word_LetterSoundCorrespondence` RENAME TO `Word_LetterSound`;
