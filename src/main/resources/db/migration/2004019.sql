15097ee52660ba7b7c030a8bc8e61049bf898d21
Alexander <alexanderkalarickal@gmail.com>
2024-07-23 21:00:20 +0530

    schema update on mvn jetty:run

--- src/main/resources/db/migration/2004019.sql
+++ src/main/resources/db/migration/2004019.sql
@@ -1,6 +1,3 @@
 # 2.4.19
 ALTER TABLE `Device` DROP COLUMN `deviceId`;
 ALTER TABLE `Device` CHANGE `deviceId` `androidId` VARCHAR(255);
-
-ALTER TABLE `Device` DROP INDEX `UK_ktkbd0xm3q2nddw1xxtdaxjy7`;
-ALTER TABLE `Device` ADD  CONSTRAINT `UK_ktkbd0xm3q2nddw1xxtdaxjy7` UNIQUE (`androidId`);
