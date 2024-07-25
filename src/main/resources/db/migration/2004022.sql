3a8f9c64adee66a22cc0f12ebfc010de0a7a6ff7
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2024-07-25 16:13:25 +0700

    chore(db): correct db migration script
    refs #1677

--- src/main/resources/db/migration/2004022.sql
+++ src/main/resources/db/migration/2004022.sql
@@ -6,15 +6,11 @@ ALTER TABLE `LetterSoundCorrespondence` RENAME TO `LetterSound`;
 
 DROP TABLE IF EXISTS `LetterSound_Letter`;
 ALTER TABLE `LetterSoundCorrespondence_Letter` RENAME TO `LetterSound_Letter`;
+ALTER TABLE `LetterSound_Letter` CHANGE `LetterSoundCorrespondence_id` `LetterSound_id` bigint(20) NOT NULL;
 
 DROP TABLE IF EXISTS `LetterSound_Sound`;
 ALTER TABLE `LetterSoundCorrespondence_Sound` RENAME TO `LetterSound_Sound`;
+ALTER TABLE `LetterSound_Sound` CHANGE `LetterSoundCorrespondence_id` `LetterSound_id` bigint(20) NOT NULL;
 
 DROP TABLE IF EXISTS `Word_LetterSound`;
 ALTER TABLE `Word_LetterSoundCorrespondence` RENAME TO `Word_LetterSound`;
-
-ALTER TABLE `LetterSound_Letter` DROP COLUMN `LetterSound_id`;
-ALTER TABLE `LetterSound_Letter` CHANGE `LetterSoundCorrespondence_id` `LetterSound_id` bigint(20) NOT NULL;
-
-ALTER TABLE `LetterSound_Sound` DROP COLUMN `LetterSound_id`;
-ALTER TABLE `LetterSound_Sound` CHANGE `LetterSoundCorrespondence_id` `LetterSound_id` bigint(20) NOT NULL;
