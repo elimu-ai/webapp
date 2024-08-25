176b6d6c3d48c13168c7ad2a0d900d0f523ca767
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2024-08-25 09:50:07 +0700

    refactor: visualize storybook learning events
    #411

--- src/main/java/ai/elimu/web/analytics/StoryBookLearningEventListController.java
+++ src/main/java/ai/elimu/web/analytics/StoryBookLearningEventListController.java
@@ -6,7 +6,10 @@ import ai.elimu.model.analytics.StoryBookLearningEvent;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,26 +38,26 @@ public class StoryBookLearningEventListController {
         List<String> monthList = new ArrayList<>();
         List<Integer> eventCountList = new ArrayList<>();
         if (!storyBookLearningEvents.isEmpty()) {
+            // Group event count by month (e.g. "Aug-2024", "Sep-2024")
+            Map<String, Integer> eventCountByMonthMap = new HashMap<>();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM-yyyy");
+            for (StoryBookLearningEvent event : storyBookLearningEvents) {
+                String eventMonth = simpleDateFormat.format(event.getTimestamp().getTime());
+                eventCountByMonthMap.put(eventMonth, eventCountByMonthMap.getOrDefault(eventMonth, 0) + 1);
+            }
+            
+            // Iterate each month from 4 years ago until now
             Calendar calendar4YearsAgo = Calendar.getInstance();
             calendar4YearsAgo.add(Calendar.YEAR, -4);
-
             Calendar calendarNow = Calendar.getInstance();
-            
             Calendar month = calendar4YearsAgo;
             while (!month.after(calendarNow)) {
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM-yyyy");
                 String monthAsString = simpleDateFormat.format(month.getTime());
                 monthList.add(monthAsString);
+                
+                eventCountList.add(eventCountByMonthMap.getOrDefault(monthAsString, 0));
 
-                int eventCount = 0;
-                for (StoryBookLearningEvent storyBookLearningEvent : storyBookLearningEvents) {
-                    String eventMonthAsString = simpleDateFormat.format(storyBookLearningEvent.getTimestamp().getTime());
-                    if (eventMonthAsString.equals(monthAsString)) {
-                        eventCount++;
-                    }
-                }
-                eventCountList.add(eventCount);
-
+                // Increase the date by 1 month
                 month.add(Calendar.MONTH, 1);
             }
         }
