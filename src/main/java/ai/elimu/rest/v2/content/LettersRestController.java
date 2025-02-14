7d9b7c8af864b1ffd577efcf9f7ebe5afc6b50bc
Ashish Bagdane <ashishbagdaneofficial@gmail.com>
2025-02-14 20:05:28 +0530

    refactor: make use of constructor injection in content controllers (closes #2012)

--- src/main/java/ai/elimu/rest/v2/content/LettersRestController.java
+++ src/main/java/ai/elimu/rest/v2/content/LettersRestController.java
@@ -5,6 +5,7 @@ import ai.elimu.model.content.Letter;
 import ai.elimu.model.v2.gson.content.LetterGson;
 import ai.elimu.rest.v2.JpaToGsonConverter;
 import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONArray;
@@ -17,26 +18,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/rest/v2/content/letters", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@RequiredArgsConstructor
 public class LettersRestController {
-    
-    private Logger logger = LogManager.getLogger();
-    
-    @Autowired
-    private LetterDao letterDao;
-    
-    @RequestMapping(method = RequestMethod.GET)
-    public String handleGetRequest() {
-        logger.info("handleGetRequest");
-        
-        JSONArray lettersJsonArray = new JSONArray();
-        for (Letter letter : letterDao.readAllOrdered()) {
-            LetterGson letterGson = JpaToGsonConverter.getLetterGson(letter);
-            String json = new Gson().toJson(letterGson);
-            lettersJsonArray.put(new JSONObject(json));
-        }
-        
-        String jsonResponse = lettersJsonArray.toString();
-        logger.info("jsonResponse: " + jsonResponse);
-        return jsonResponse;
+
+  private Logger logger = LogManager.getLogger();
+
+  @Autowired
+  private final LetterDao letterDao;
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String handleGetRequest() {
+    logger.info("handleGetRequest");
+
+    JSONArray lettersJsonArray = new JSONArray();
+    for (Letter letter : letterDao.readAllOrdered()) {
+      LetterGson letterGson = JpaToGsonConverter.getLetterGson(letter);
+      String json = new Gson().toJson(letterGson);
+      lettersJsonArray.put(new JSONObject(json));
     }
+
+    String jsonResponse = lettersJsonArray.toString();
+    logger.info("jsonResponse: " + jsonResponse);
+    return jsonResponse;
+  }
 }
--- src/main/java/ai/elimu/rest/v2/content/WordsRestController.java
+++ src/main/java/ai/elimu/rest/v2/content/WordsRestController.java
@@ -5,6 +5,7 @@ import ai.elimu.model.content.Word;
 import ai.elimu.model.v2.gson.content.WordGson;
 import ai.elimu.rest.v2.JpaToGsonConverter;
 import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONArray;
@@ -17,26 +18,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/rest/v2/content/words", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@RequiredArgsConstructor
 public class WordsRestController {
-    
-    private Logger logger = LogManager.getLogger();
-    
-    @Autowired
-    private WordDao wordDao;
-    
-    @RequestMapping(method = RequestMethod.GET)
-    public String handleGetRequest() {
-        logger.info("handleGetRequest");
-        
-        JSONArray wordsJsonArray = new JSONArray();
-        for (Word word : wordDao.readAllOrdered()) {
-            WordGson wordGson = JpaToGsonConverter.getWordGson(word);
-            String json = new Gson().toJson(wordGson);
-            wordsJsonArray.put(new JSONObject(json));
-        }
-        
-        String jsonResponse = wordsJsonArray.toString();
-        logger.info("jsonResponse: " + jsonResponse);
-        return jsonResponse;
+
+  private Logger logger = LogManager.getLogger();
+
+  @Autowired
+  private final WordDao wordDao;
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String handleGetRequest() {
+    logger.info("handleGetRequest");
+
+    JSONArray wordsJsonArray = new JSONArray();
+    for (Word word : wordDao.readAllOrdered()) {
+      WordGson wordGson = JpaToGsonConverter.getWordGson(word);
+      String json = new Gson().toJson(wordGson);
+      wordsJsonArray.put(new JSONObject(json));
     }
+
+    String jsonResponse = wordsJsonArray.toString();
+    logger.info("jsonResponse: " + jsonResponse);
+    return jsonResponse;
+  }
 }
