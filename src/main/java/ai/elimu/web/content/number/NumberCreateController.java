62506fb03cb49679d2ed01d4ceb8944015009d42
Ashish Bagdane <ashishbagdaneofficial@gmail.com>
2025-02-14 21:44:09 +0530

    refactor: make use of constructor injection in web content number controllers (closes #2012)

--- src/main/java/ai/elimu/web/content/number/NumberCreateController.java
+++ src/main/java/ai/elimu/web/content/number/NumberCreateController.java
@@ -2,11 +2,6 @@ package ai.elimu.web.content.number;
 
 import ai.elimu.dao.EmojiDao;
 import ai.elimu.dao.NumberContributionEventDao;
-import java.util.Calendar;
-import java.util.List;
-import jakarta.validation.Valid;
-
-import org.apache.logging.log4j.Logger;
 import ai.elimu.dao.NumberDao;
 import ai.elimu.dao.WordDao;
 import ai.elimu.model.content.Emoji;
@@ -16,13 +11,17 @@ import ai.elimu.model.contributor.Contributor;
 import ai.elimu.model.contributor.NumberContributionEvent;
 import ai.elimu.util.DiscordHelper;
 import ai.elimu.web.context.EnvironmentContextLoaderListener;
-import java.util.HashMap;
-import java.util.Map;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -31,107 +30,104 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/content/number/create")
+@RequiredArgsConstructor
 public class NumberCreateController {
-    
-    private final Logger logger = LogManager.getLogger();
-    
-    @Autowired
-    private NumberDao numberDao;
-    
-    @Autowired
-    private NumberContributionEventDao numberContributionEventDao;
-    
-    @Autowired
-    private WordDao wordDao;
-    
-    @Autowired
-    private EmojiDao emojiDao;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String handleRequest(
-            Model model) {
-        logger.info("handleRequest");
-        
-        Number number = new Number();
-        model.addAttribute("number", number);
-        
-        model.addAttribute("timeStart", System.currentTimeMillis());
-        
-        model.addAttribute("words", wordDao.readAllOrdered());
-        model.addAttribute("emojisByWordId", getEmojisByWordId());
-        
-        return "content/number/create";
+  private final Logger logger = LogManager.getLogger();
+
+  private final NumberDao numberDao;
+
+  private final NumberContributionEventDao numberContributionEventDao;
+
+  private final WordDao wordDao;
+
+  private final EmojiDao emojiDao;
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String handleRequest(
+      Model model) {
+    logger.info("handleRequest");
+
+    Number number = new Number();
+    model.addAttribute("number", number);
+
+    model.addAttribute("timeStart", System.currentTimeMillis());
+
+    model.addAttribute("words", wordDao.readAllOrdered());
+    model.addAttribute("emojisByWordId", getEmojisByWordId());
+
+    return "content/number/create";
+  }
+
+  @RequestMapping(method = RequestMethod.POST)
+  public String handleSubmit(
+      HttpServletRequest request,
+      HttpSession session,
+      @Valid Number number,
+      BindingResult result,
+      Model model) {
+    logger.info("handleSubmit");
+
+    Number existingNumber = numberDao.readByValue(number.getValue());
+    if (existingNumber != null) {
+      result.rejectValue("value", "NonUnique");
     }
-    
-    @RequestMapping(method = RequestMethod.POST)
-    public String handleSubmit(
-            HttpServletRequest request,
-            HttpSession session,
-            @Valid Number number,
-            BindingResult result,
-            Model model) {
-        logger.info("handleSubmit");
-        
-        Number existingNumber = numberDao.readByValue(number.getValue());
-        if (existingNumber != null) {
-            result.rejectValue("value", "NonUnique");
-        }
-        
-        if (result.hasErrors()) {
-            model.addAttribute("number", number);
-            
-            model.addAttribute("timeStart", request.getParameter("timeStart"));
-            
-            model.addAttribute("words", wordDao.readAllOrdered());
-            model.addAttribute("emojisByWordId", getEmojisByWordId());
-            
-            return "content/number/create";
-        } else {            
-            number.setTimeLastUpdate(Calendar.getInstance());
-            numberDao.create(number);
-            
-            NumberContributionEvent numberContributionEvent = new NumberContributionEvent();
-            numberContributionEvent.setContributor((Contributor) session.getAttribute("contributor"));
-            numberContributionEvent.setTimestamp(Calendar.getInstance());
-            numberContributionEvent.setNumber(number);
-            numberContributionEvent.setRevisionNumber(number.getRevisionNumber());
-            numberContributionEvent.setComment(StringUtils.abbreviate(request.getParameter("contributionComment"), 1000));
-            numberContributionEvent.setTimeSpentMs(System.currentTimeMillis() - Long.valueOf(request.getParameter("timeStart")));
-            numberContributionEventDao.create(numberContributionEvent);
-            
-            if (!EnvironmentContextLoaderListener.PROPERTIES.isEmpty()) {
-                String contentUrl = "https://" + EnvironmentContextLoaderListener.PROPERTIES.getProperty("content.language").toLowerCase() + ".elimu.ai/content/number/edit/" + number.getId();
-                DiscordHelper.sendChannelMessage(
-                        "Number created: " + contentUrl,
-                        String.valueOf(numberContributionEvent.getNumber().getValue()),
-                        "Comment: \"" + numberContributionEvent.getComment() + "\"",
-                        null,
-                        null
-                );
-            }
-            
-            return "redirect:/content/number/list#" + number.getId();
-        }
+
+    if (result.hasErrors()) {
+      model.addAttribute("number", number);
+
+      model.addAttribute("timeStart", request.getParameter("timeStart"));
+
+      model.addAttribute("words", wordDao.readAllOrdered());
+      model.addAttribute("emojisByWordId", getEmojisByWordId());
+
+      return "content/number/create";
+    } else {
+      number.setTimeLastUpdate(Calendar.getInstance());
+      numberDao.create(number);
+
+      NumberContributionEvent numberContributionEvent = new NumberContributionEvent();
+      numberContributionEvent.setContributor((Contributor) session.getAttribute("contributor"));
+      numberContributionEvent.setTimestamp(Calendar.getInstance());
+      numberContributionEvent.setNumber(number);
+      numberContributionEvent.setRevisionNumber(number.getRevisionNumber());
+      numberContributionEvent.setComment(StringUtils.abbreviate(request.getParameter("contributionComment"), 1000));
+      numberContributionEvent.setTimeSpentMs(System.currentTimeMillis() - Long.valueOf(request.getParameter("timeStart")));
+      numberContributionEventDao.create(numberContributionEvent);
+
+      if (!EnvironmentContextLoaderListener.PROPERTIES.isEmpty()) {
+        String contentUrl = "https://" + EnvironmentContextLoaderListener.PROPERTIES.getProperty("content.language").toLowerCase() + ".elimu.ai/content/number/edit/" + number.getId();
+        DiscordHelper.sendChannelMessage(
+            "Number created: " + contentUrl,
+            String.valueOf(numberContributionEvent.getNumber().getValue()),
+            "Comment: \"" + numberContributionEvent.getComment() + "\"",
+            null,
+            null
+        );
+      }
+
+      return "redirect:/content/number/list#" + number.getId();
     }
-    
-    private Map<Long, String> getEmojisByWordId() {
-        logger.info("getEmojisByWordId");
-        
-        Map<Long, String> emojisByWordId = new HashMap<>();
-        
-        for (Word word : wordDao.readAll()) {
-            String emojiGlyphs = "";
-            
-            List<Emoji> emojis = emojiDao.readAllLabeled(word);
-            for (Emoji emoji : emojis) {
-                emojiGlyphs += emoji.getGlyph();
-            }
-            
-            if (StringUtils.isNotBlank(emojiGlyphs)) {
-                emojisByWordId.put(word.getId(), emojiGlyphs);
-            }
-        }
-        
-        return emojisByWordId;
+  }
+
+  private Map<Long, String> getEmojisByWordId() {
+    logger.info("getEmojisByWordId");
+
+    Map<Long, String> emojisByWordId = new HashMap<>();
+
+    for (Word word : wordDao.readAll()) {
+      String emojiGlyphs = "";
+
+      List<Emoji> emojis = emojiDao.readAllLabeled(word);
+      for (Emoji emoji : emojis) {
+        emojiGlyphs += emoji.getGlyph();
+      }
+
+      if (StringUtils.isNotBlank(emojiGlyphs)) {
+        emojisByWordId.put(word.getId(), emojiGlyphs);
+      }
     }
+
+    return emojisByWordId;
+  }
 }
