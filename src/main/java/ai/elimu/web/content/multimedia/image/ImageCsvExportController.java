ce2754a7e935a178db79d6fc7341b3fe1f235c91
Ashish Bagdane <ashishbagdaneofficial@gmail.com>
2025-02-14 21:25:08 +0530

    refactor: make use of constructor injection in web content multimedia image controllers (closes #2012)

--- src/main/java/ai/elimu/web/content/multimedia/image/ImageCsvExportController.java
+++ src/main/java/ai/elimu/web/content/multimedia/image/ImageCsvExportController.java
@@ -2,82 +2,79 @@ package ai.elimu.web.content.multimedia.image;
 
 import ai.elimu.dao.ImageDao;
 import ai.elimu.model.content.multimedia.Image;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.StringWriter;
 import java.util.List;
-
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.servlet.http.HttpServletResponse;
-
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/content/image/list")
+@RequiredArgsConstructor
 public class ImageCsvExportController {
-    
-    private final Logger logger = LogManager.getLogger();
-    
-    @Autowired
-    private ImageDao imageDao;
-    
-    @RequestMapping(value="/images.csv", method = RequestMethod.GET)
-    public void handleRequest(
-            HttpServletResponse response,
-            OutputStream outputStream) throws IOException {
-        logger.info("handleRequest");
 
-        List<Image> images = imageDao.readAllOrderedById();
-        logger.info("images.size(): " + images.size());
+  private final Logger logger = LogManager.getLogger();
 
-        CSVFormat csvFormat = CSVFormat.DEFAULT
-                .withHeader(
-                        "id", 
-                        "content_type", 
-                        "content_license", 
-                        "attribution_url", 
-                        "title",
-                        "cid",
-                        "download_url",
-                        "image_format"
-                );
-        StringWriter stringWriter = new StringWriter();
-        CSVPrinter csvPrinter = new CSVPrinter(stringWriter, csvFormat);
+  private final ImageDao imageDao;
 
-        for (Image image : images) {
-            csvPrinter.printRecord(
-                image.getId(),
-                image.getContentType(),
-                image.getContentLicense(),
-                image.getAttributionUrl(),
-                image.getTitle(),
-                image.getCid(),
-                image.getUrl(),
-                image.getImageFormat()
-            );
-            
-            csvPrinter.flush();
-        }
-        csvPrinter.close();
+  @RequestMapping(value = "/images.csv", method = RequestMethod.GET)
+  public void handleRequest(
+      HttpServletResponse response,
+      OutputStream outputStream) throws IOException {
+    logger.info("handleRequest");
 
-        String csvFileContent = stringWriter.toString();
-        
-        response.setContentType("text/csv");
-        byte[] bytes = csvFileContent.getBytes();
-        response.setContentLength(bytes.length);
-        try {
-            outputStream.write(bytes);
-            outputStream.flush();
-            outputStream.close();
-        } catch (IOException ex) {
-            logger.error(ex);
-        }
+    List<Image> images = imageDao.readAllOrderedById();
+    logger.info("images.size(): " + images.size());
+
+    CSVFormat csvFormat = CSVFormat.DEFAULT
+        .withHeader(
+            "id",
+            "content_type",
+            "content_license",
+            "attribution_url",
+            "title",
+            "cid",
+            "download_url",
+            "image_format"
+        );
+    StringWriter stringWriter = new StringWriter();
+    CSVPrinter csvPrinter = new CSVPrinter(stringWriter, csvFormat);
+
+    for (Image image : images) {
+      csvPrinter.printRecord(
+          image.getId(),
+          image.getContentType(),
+          image.getContentLicense(),
+          image.getAttributionUrl(),
+          image.getTitle(),
+          image.getCid(),
+          image.getUrl(),
+          image.getImageFormat()
+      );
+
+      csvPrinter.flush();
     }
+    csvPrinter.close();
+
+    String csvFileContent = stringWriter.toString();
+
+    response.setContentType("text/csv");
+    byte[] bytes = csvFileContent.getBytes();
+    response.setContentLength(bytes.length);
+    try {
+      outputStream.write(bytes);
+      outputStream.flush();
+      outputStream.close();
+    } catch (IOException ex) {
+      logger.error(ex);
+    }
+  }
 }
