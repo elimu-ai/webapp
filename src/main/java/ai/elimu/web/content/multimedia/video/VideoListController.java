6cf25abbcf37cd430f6413ad246323dc4c8e4db5
Ashish Bagdane <ashishbagdaneofficial@gmail.com>
2025-02-14 21:29:56 +0530

    refactor: make use of constructor injection in web content multimedia video controllers (closes #2012)

--- src/main/java/ai/elimu/web/content/multimedia/video/VideoListController.java
+++ src/main/java/ai/elimu/web/content/multimedia/video/VideoListController.java
@@ -1,12 +1,11 @@
 package ai.elimu.web.content.multimedia.video;
 
-import java.util.List;
-
-import org.apache.logging.log4j.Logger;
 import ai.elimu.dao.VideoDao;
 import ai.elimu.model.content.multimedia.Video;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,20 +13,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/content/multimedia/video/list")
+@RequiredArgsConstructor
 public class VideoListController {
-    
-    private final Logger logger = LogManager.getLogger();
-    
-    @Autowired
-    private VideoDao videoDao;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String handleRequest(Model model) {
-        logger.info("handleRequest");
-        
-        List<Video> videos = videoDao.readAllOrdered();
-        model.addAttribute("videos", videos);
+  private final Logger logger = LogManager.getLogger();
+    
+  private final VideoDao videoDao;
 
-        return "content/multimedia/video/list";
-    }
+  @RequestMapping(method = RequestMethod.GET)
+  public String handleRequest(Model model) {
+    logger.info("handleRequest");
+
+    List<Video> videos = videoDao.readAllOrdered();
+    model.addAttribute("videos", videos);
+
+    return "content/multimedia/video/list";
+  }
 }
