8f3ec10ce41d66a20f0b4a1bfa7e67ac54178267
Ashish Bagdane <ashishbagdaneofficial@gmail.com>
2025-02-14 23:04:08 +0530

    refactor: make use of constructor injection in web contributor controllers (closes #2012)

--- src/main/java/ai/elimu/web/contributor/ContributorListController.java
+++ src/main/java/ai/elimu/web/contributor/ContributorListController.java
@@ -1,9 +1,9 @@
 package ai.elimu.web.contributor;
 
 import ai.elimu.dao.ContributorDao;
-import org.apache.logging.log4j.Logger;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +11,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/contributor/list")
+@RequiredArgsConstructor
 public class ContributorListController {
-    
-    private final Logger logger = LogManager.getLogger();
-    
-    @Autowired
-    private ContributorDao contributorDao;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String handleRequest(Model model) {
-        logger.info("handleRequest");
-        
-        model.addAttribute("contributors", contributorDao.readAllOrderedDesc());
+  private final Logger logger = LogManager.getLogger();
 
-        return "contributor/list";
-    }
+  private final ContributorDao contributorDao;
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String handleRequest(Model model) {
+    logger.info("handleRequest");
+
+    model.addAttribute("contributors", contributorDao.readAllOrderedDesc());
+
+    return "contributor/list";
+  }
 }
