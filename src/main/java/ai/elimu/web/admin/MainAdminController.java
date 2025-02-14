f50add8dc04bf694aa6339c16198d6bc6a4e0869
Ashish Bagdane <ashishbagdaneofficial@gmail.com>
2025-02-14 20:43:14 +0530

    refactor: make use of constructor injection in web admin controllers (closes #2012)

--- src/main/java/ai/elimu/web/admin/MainAdminController.java
+++ src/main/java/ai/elimu/web/admin/MainAdminController.java
@@ -1,9 +1,9 @@
 package ai.elimu.web.admin;
 
 import ai.elimu.dao.ApplicationDao;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +11,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class MainAdminController {
-    
-    private final Logger logger = LogManager.getLogger();
-    
-    @Autowired
-    private ApplicationDao applicationDao;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String handleRequest(Model model) {
-        logger.info("handleRequest");
-        
-        model.addAttribute("applicationCount", applicationDao.readCount());
-        
-        return "admin/main";
-    }
+  private final Logger logger = LogManager.getLogger();
+
+  private final ApplicationDao applicationDao;
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String handleRequest(Model model) {
+    logger.info("handleRequest");
+
+    model.addAttribute("applicationCount", applicationDao.readCount());
+
+    return "admin/main";
+  }
 }
--- src/main/java/ai/elimu/web/admin/application/ApplicationCreateController.java
+++ src/main/java/ai/elimu/web/admin/application/ApplicationCreateController.java
@@ -1,16 +1,15 @@
 package ai.elimu.web.admin.application;
 
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
-
-import org.apache.logging.log4j.Logger;
 import ai.elimu.dao.ApplicationDao;
 import ai.elimu.model.admin.Application;
+import ai.elimu.model.v2.enums.admin.ApplicationStatus;
 import ai.elimu.model.v2.enums.content.LiteracySkill;
 import ai.elimu.model.v2.enums.content.NumeracySkill;
-import ai.elimu.model.v2.enums.admin.ApplicationStatus;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,49 +18,49 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/admin/application/create")
+@RequiredArgsConstructor
 public class ApplicationCreateController {
-    
-    private final Logger logger = LogManager.getLogger();
-    
-    @Autowired
-    private ApplicationDao applicationDao;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String handleRequest(Model model) {
-        logger.info("handleRequest");
-        
-        Application application = new Application();
-        application.setApplicationStatus(ApplicationStatus.MISSING_APK);
-        model.addAttribute("application", application);
-        
-        model.addAttribute("literacySkills", LiteracySkill.values());
-        model.addAttribute("numeracySkills", NumeracySkill.values());
+  private final Logger logger = LogManager.getLogger();
 
-        return "admin/application/create";
+  private final ApplicationDao applicationDao;
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String handleRequest(Model model) {
+    logger.info("handleRequest");
+
+    Application application = new Application();
+    application.setApplicationStatus(ApplicationStatus.MISSING_APK);
+    model.addAttribute("application", application);
+
+    model.addAttribute("literacySkills", LiteracySkill.values());
+    model.addAttribute("numeracySkills", NumeracySkill.values());
+
+    return "admin/application/create";
+  }
+
+  @RequestMapping(method = RequestMethod.POST)
+  public String handleSubmit(
+      HttpSession session,
+      @Valid Application application,
+      BindingResult result,
+      Model model
+  ) {
+    logger.info("handleSubmit");
+
+    Application existingApplication = applicationDao.readByPackageName(application.getPackageName());
+    if (existingApplication != null) {
+      result.rejectValue("packageName", "NonUnique");
     }
-    
-    @RequestMapping(method = RequestMethod.POST)
-    public String handleSubmit(
-            HttpSession session,
-            @Valid Application application,
-            BindingResult result,
-            Model model
-    ) {
-        logger.info("handleSubmit");
-        
-        Application existingApplication = applicationDao.readByPackageName(application.getPackageName());
-        if (existingApplication != null) {
-            result.rejectValue("packageName", "NonUnique");
-        }
-        
-        if (result.hasErrors()) {
-            model.addAttribute("application", application);
-            model.addAttribute("literacySkills", LiteracySkill.values());
-            model.addAttribute("numeracySkills", NumeracySkill.values());
-            return "admin/application/create";
-        } else {
-            applicationDao.create(application);
-            return "redirect:/admin/application/list#" + application.getId();
-        }
+
+    if (result.hasErrors()) {
+      model.addAttribute("application", application);
+      model.addAttribute("literacySkills", LiteracySkill.values());
+      model.addAttribute("numeracySkills", NumeracySkill.values());
+      return "admin/application/create";
+    } else {
+      applicationDao.create(application);
+      return "redirect:/admin/application/list#" + application.getId();
     }
+  }
 }
