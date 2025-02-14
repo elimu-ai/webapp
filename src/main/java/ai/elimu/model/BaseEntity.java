9465e80a5aa0184b1d0aca917b15f195bcc37e9b
Ashish Bagdane <ashishbagdaneofficial@gmail.com>
2025-02-14 15:15:12 +0530

    refactor: make use of lombok annotations for getters and setters for base models (closes #2012)

--- src/main/java/ai/elimu/model/BaseEntity.java
+++ src/main/java/ai/elimu/model/BaseEntity.java
@@ -1,25 +1,21 @@
 package ai.elimu.model;
 
-import java.io.Serializable;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class BaseEntity implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  private static final long serialVersionUID = 1L;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 }
