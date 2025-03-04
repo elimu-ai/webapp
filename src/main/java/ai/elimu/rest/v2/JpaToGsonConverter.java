71df9fc713443bfa42633df4d6427a656b29be1e
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-03-04 21:41:34 +0700

    refactor: remove audio - audio gson
    #1944

--- src/main/java/ai/elimu/rest/v2/JpaToGsonConverter.java
+++ src/main/java/ai/elimu/rest/v2/JpaToGsonConverter.java
@@ -17,7 +17,6 @@ import ai.elimu.model.content.multimedia.Video;
 import ai.elimu.model.v2.gson.application.ApplicationGson;
 import ai.elimu.model.v2.gson.application.ApplicationVersionGson;
 import ai.elimu.model.v2.gson.content.SoundGson;
-import ai.elimu.model.v2.gson.content.AudioGson;
 import ai.elimu.model.v2.gson.content.EmojiGson;
 import ai.elimu.model.v2.gson.content.ImageGson;
 import ai.elimu.model.v2.gson.content.LetterGson;
@@ -217,31 +216,6 @@ public class JpaToGsonConverter {
             imageGson.setWords(wordGsons);
             
             return imageGson;
-        }
-    }
-    
-    public static AudioGson getAudioGson(Audio audio) {
-        if (audio == null) {
-            return null;
-        } else {
-            AudioGson audioGson = new AudioGson();
-            
-            // BaseEntity
-            audioGson.setId(audio.getId());
-            
-            // Content
-            audioGson.setRevisionNumber(audio.getRevisionNumber());
-            audioGson.setUsageCount(audio.getUsageCount());
-            
-            // Audio
-            audioGson.setTitle(audio.getTitle());
-            audioGson.setTranscription(audio.getTranscription());
-            audioGson.setAudioFormat(audio.getAudioFormat());
-            audioGson.setBytesUrl("/audio/" + audio.getId() + "_r" + audio.getRevisionNumber() + "." + audio.getAudioFormat().toString().toLowerCase());
-            audioGson.setBytesSize(audio.getBytes().length / 1024);
-            audioGson.setDurationMs(audio.getDurationMs());
-            
-            return audioGson;
         }
     }
     
