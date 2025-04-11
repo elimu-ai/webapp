18d3eed20c536d7478495d7def6d30047d6dd4a7
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-04-11 12:10:16 +0700

    refactor: add missing final
    #2118

--- src/main/java/ai/elimu/web/analytics/MainAnalyticsController.java
+++ src/main/java/ai/elimu/web/analytics/MainAnalyticsController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class MainAnalyticsController {
 
-  private LetterSoundLearningEventDao letterSoundLearningEventDao;
+  private final LetterSoundLearningEventDao letterSoundLearningEventDao;
 
   private final WordLearningEventDao wordLearningEventDao;
 
