bcfe5cdf994875d7651172b8fe3023f9edf3bfe3
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-04-18 16:09:11 +0700

    refactor(images): temporary fix until proper implementation
    #2126

--- src/main/java/ai/elimu/entity/content/multimedia/Image.java
+++ src/main/java/ai/elimu/entity/content/multimedia/Image.java
@@ -2,6 +2,7 @@ package ai.elimu.entity.content.multimedia;
 
 import ai.elimu.model.v2.enums.content.ImageFormat;
 import ai.elimu.web.context.EnvironmentContextLoaderListener;
+import io.micrometer.common.util.StringUtils;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -43,9 +44,13 @@ public class Image extends Multimedia {
   private String dominantColor; // Web color
 
   public String getUrl() {
+    String filename = getChecksumMd5() + "." + getImageFormat().toString().toLowerCase();
+    if (StringUtils.isBlank(getCid())) {
+      filename = getId() + "_r" + getRevisionNumber() + "." + getImageFormat().toString().toLowerCase();
+    }
     return "https://raw.githubusercontent.com/elimu-ai/webapp-lfs/main" +
         "/lang-" + EnvironmentContextLoaderListener.PROPERTIES.getProperty("content.language") +
         "/images" +
-        "/" + getChecksumMd5() + "." + getImageFormat().toString().toLowerCase();
+        "/" + filename;
   }
 }
