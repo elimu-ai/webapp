8b761f33ec1e2f0f9b1ee9c1a823a439dfa20095
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-05-10 16:42:06 +0700

    feat: generate student id during data collection
    #2103

--- src/main/java/ai/elimu/web/analytics/students/StudentsListController.java
+++ src/main/java/ai/elimu/web/analytics/students/StudentsListController.java
@@ -1,6 +1,10 @@
 package ai.elimu.web.analytics.students;
 
+import ai.elimu.dao.StoryBookLearningEventDao;
 import ai.elimu.dao.StudentDao;
+import ai.elimu.dao.WordLearningEventDao;
+import ai.elimu.entity.analytics.StoryBookLearningEvent;
+import ai.elimu.entity.analytics.WordLearningEvent;
 import ai.elimu.entity.analytics.students.Student;
 import ai.elimu.util.AnalyticsHelper;
 import lombok.RequiredArgsConstructor;
@@ -21,9 +25,35 @@ public class StudentsListController {
 
   private final StudentDao studentDao;
 
+  private final WordLearningEventDao wordLearningEventDao;
+
+  private final StoryBookLearningEventDao storyBookLearningEventDao;
+
   @GetMapping
   public String handleRequest(Model model) {
     log.info("handleRequest");
+
+    // Generate Student IDs for pre-existing learning events
+    for (WordLearningEvent wordLearningEvent : wordLearningEventDao.readAll()) {
+      log.info("wordLearningEvent.getAndroidId(): " + wordLearningEvent.getAndroidId());
+      Student existingStudent = studentDao.read(wordLearningEvent.getAndroidId());
+      if (existingStudent == null) {
+        Student student = new Student();
+        student.setAndroidId(wordLearningEvent.getAndroidId());
+        studentDao.create(student);
+        log.info("Stored Student in database with ID " + student.getId());
+      }
+    }
+    for (StoryBookLearningEvent storyBookLearningEvent : storyBookLearningEventDao.readAll()) {
+      log.info("storyBookLearningEvent.getAndroidId(): " + storyBookLearningEvent.getAndroidId());
+      Student existingStudent = studentDao.read(storyBookLearningEvent.getAndroidId());
+      if (existingStudent == null) {
+        Student student = new Student();
+        student.setAndroidId(storyBookLearningEvent.getAndroidId());
+        studentDao.create(student);
+        log.info("Stored Student in database with ID " + student.getId());
+      }
+    }
     
     List<Student> students = studentDao.readAll();
     for (Student student : students) {
