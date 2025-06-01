4282293d7ef437f88179f560d2964f2968beadaf
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-06-01 23:05:42 +0700

    fix: nullpointer in word learning event import scheduler
    #2208

--- src/main/java/ai/elimu/dao/jpa/WordLearningEventDaoJpa.java
+++ src/main/java/ai/elimu/dao/jpa/WordLearningEventDaoJpa.java
@@ -2,7 +2,6 @@ package ai.elimu.dao.jpa;
 
 import ai.elimu.dao.WordLearningEventDao;
 import ai.elimu.entity.analytics.WordLearningEvent;
-import ai.elimu.entity.application.Application;
 import ai.elimu.entity.content.Word;
 
 import java.util.Calendar;
@@ -14,22 +13,22 @@ import org.springframework.dao.DataAccessException;
 public class WordLearningEventDaoJpa extends GenericDaoJpa<WordLearningEvent> implements WordLearningEventDao {
 
     @Override
-    public WordLearningEvent read(Calendar timestamp, String androidId, Application application, Word word) throws DataAccessException {
+    public WordLearningEvent read(Calendar timestamp, String androidId, String packageName, Word word) throws DataAccessException {
         try {
             return (WordLearningEvent) em.createQuery(
                 "SELECT wle " +
                 "FROM WordLearningEvent wle " +
                 "WHERE wle.timestamp = :timestamp " +
                 "AND wle.androidId = :androidId " + 
-                "AND wle.application = :application " + 
+                "AND wle.packageName = :packageName " + 
                 "AND wle.word = :word")
                 .setParameter("timestamp", timestamp)
                 .setParameter("androidId", androidId)
-                .setParameter("application", application)
+                .setParameter("packageName", packageName)
                 .setParameter("word", word)
                 .getSingleResult();
         } catch (NoResultException e) {
-            logger.info("WordLearningEvent (" + timestamp.getTimeInMillis() + ", " + androidId + ", " + application.getPackageName() + ", \"" + word.getText() + "\") was not found");
+            logger.info("WordLearningEvent (" + timestamp.getTimeInMillis() + ", " + androidId + ", \"" + packageName + "\", \"" + word.getText() + "\") was not found");
             return null;
         }
     }
