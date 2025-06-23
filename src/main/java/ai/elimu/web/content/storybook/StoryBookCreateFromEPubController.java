993dea88b509811b4df1e08380bba1e309050970
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-06-23 22:41:53 +0700

    fix: exclude chapters containing book metadata

--- src/main/java/ai/elimu/web/content/storybook/StoryBookCreateFromEPubController.java
+++ src/main/java/ai/elimu/web/content/storybook/StoryBookCreateFromEPubController.java
@@ -378,7 +378,7 @@ public class StoryBookCreateFromEPubController {
                   || originalTextLowerCase.contains("pratham books")
                   || originalTextLowerCase.contains("storyweaver")
                   || originalTextLowerCase.contains("copyright page")
-                  || originalTextLowerCase.contains("by smart axiata") // StoryBookProvider#LETS_READ_ASIA
+                  || originalTextLowerCase.contains("smart axiata") // StoryBookProvider#LETS_READ_ASIA
                   || originalTextLowerCase.contains("the asia foundation") // StoryBookProvider#LETS_READ_ASIA
                   || originalTextLowerCase.contains("এশিয়া ফাউন্ডেশনের") // StoryBookProvider#LETS_READ_ASIA
           ) {
